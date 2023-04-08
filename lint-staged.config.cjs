385cda3d476d51b53c19b9041b25c32c108c5e0b
chensuiyi <bimostyle@qq.com>
2023-04-08 08:44:58 +0800

    【初次提交】: 第一次提交，测试发包

---
+++ lint-staged.config.cjs
@@ -0,0 +1,3 @@
+module.exports = {
+    '*.{js,css,scss,less,ts,jsx,vue,html,json,md,yaml}': 'prettier --write'
+};
---
+++ prettier.config.cjs
@@ -0,0 +1,18 @@
+module.exports = {
+    // 不使用尾部逗号
+    trailingComma: 'none',
+    // 一个tab表示4个空格
+    tabWidth: 4,
+    // 每个语句后添加分号
+    semi: true,
+    // 使用单引号
+    singleQuote: true,
+    // 打印宽度为80
+    printWidth: 1024,
+    // 使用括号间距
+    bracketSpacing: true,
+    // 不使用tab
+    useTabs: false,
+    // 箭头函数始终加圆括号
+    arrowParens: 'always'
+};
