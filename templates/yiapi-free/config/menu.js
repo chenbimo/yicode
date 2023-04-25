2d10536adac75b166b5e7e4c1026a7fbbb556835
chensuiyi <bimostyle@qq.com>
2023-04-25 22:28:29 +0800

    【注释说明】(yiapi-free): 增加菜单配置示例

--- templates/yiapi-free/config/menu.js
+++ templates/yiapi-free/config/menu.js
@@ -1,4 +1,25 @@
-// 初始化用到的菜单配置，请勿改动
-const menuConfig = {};
+// 菜单配置，结构参考如下所示
+const menuConfig = {
+    // '/menu1': {
+    //     name: '菜单1',
+    //     describe: '菜单1的描述',
+    //     children: {
+    //         '/sub-menu1': {
+    //             name: '菜单1的子菜单',
+    //             describe: '菜单1的子菜单描述'
+    //         }
+    //     }
+    // },
+    // '/menu2': {
+    //     name: '菜单2',
+    //     describe: '菜单2的描述',
+    //     children: {
+    //         '/sub-menu2': {
+    //             name: '菜单2的子菜单',
+    //             describe: '菜单2的子菜单描述'
+    //         }
+    //     }
+    // }
+};
 
 export { menuConfig };
