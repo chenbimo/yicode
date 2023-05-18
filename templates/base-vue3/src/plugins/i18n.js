ff0d70f9af374ef22c1522f517f646d2ea79f4e6
chensuiyi <chenbimo@gmail.com>
2023-05-18 22:17:58 +0800

    【重要补丁】(base-vue3): @intlify/unplugin-vue-i18n继续作为项目依赖

--- templates/base-vue3/src/plugins/i18n.js
+++ templates/base-vue3/src/plugins/i18n.js
@@ -1,9 +1,9 @@
 import { createI18n } from 'vue-i18n';
-import { vueI18nMessages } from '@yicode/yite-cli';
+import messages from '@intlify/unplugin-vue-i18n/messages';
 
 const i18n = createI18n({
     locale: 'zh',
-    messages: vueI18nMessages
+    messages: messages
 });
 
 export { i18n };
