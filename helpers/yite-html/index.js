a41663c9f03c5a59e53f82b5065a80e0cd50b4cf
chensuiyi <chenbimo@gmail.com>
2023-05-21 23:52:03 +0800

    【代码重构】(yite-html): 重写实现方案

--- helpers/yite-html/index.js
+++ helpers/yite-html/index.js
@@ -1,20 +1,14 @@
-import fs from 'node:fs';
-import path from 'node:path';
 export const yiteHtml = (options) => {
-    let config = {};
     return {
         name: 'yite-html',
         enforce: 'pre',
-        configResolved(resolvedConfig) {
-            config = resolvedConfig;
-        },
-        options(options) {
-            let htmlData = fs.readFileSync(path.resolve(path.dirname(options.input), 'src', 'index.html'), { encoding: 'utf8' });
-            htmlData = htmlData.replace('</body>', `<script type="module" src="/src/${process.env.NODE_ENV}.js"></script></body>`);
-            if (config.env.VITE_PROJECT_NAME) {
-                htmlData = htmlData.replace(/<title>(.*?)<\/title>/gi, `<title>${config.env.VITE_PROJECT_NAME}</title>`);
-            }
-            fs.writeFileSync(options.input, htmlData);
+        apply: 'build',
+        transformIndexHtml(html) {
+            return html.replace(
+                //
+                /<script.+dev\.js.+>.+<\/script>/,
+                '<script type="module" src="/src/build.js"></script>'
+            );
         }
     };
 };
