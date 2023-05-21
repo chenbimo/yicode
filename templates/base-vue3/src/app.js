156662c0bc79f70c8cb0ffb05f91f99d67322a6b
chensuiyi <chenbimo@gmail.com>
2023-05-21 23:52:29 +0800

    【优化调整】(base-vue3): 优化多环境html入口方案

--- templates/base-vue3/src/app.js
+++ templates/base-vue3/src/app.js
@@ -1,6 +1,9 @@
 import App from '@/App.vue';
 import 'virtual:uno.css';
 
+console.log('环境变量');
+console.log(import.meta.env.VITE_NAMESPACE);
+
 const app = createApp(App);
 
 const $Pinia = Pinia.createPinia();
