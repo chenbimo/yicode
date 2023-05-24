892af7f51cbb0763c307db01719d8973a76e41b4
chensuiyi <chenbimo@gmail.com>
2023-05-24 13:56:16 +0800

    【优化调整】(yiapi-free): 调整参数配置

--- templates/yiapi-free/config/appConfig.js
+++ templates/yiapi-free/config/appConfig.js
@@ -1,17 +1,12 @@
 let appConfig = {
-    appName: '易接口免费模板',
-    appNameEn: 'yiapi-free',
     port: 3000,
     devPassword: '123456',
-    // 禁用参数检查
-    authCheck: false,
     database: {
         // 本地数据库
         db: 'test',
         username: 'root',
         password: 'root',
         host: '127.0.0.1',
-        dialect: 'mysql',
         port: 3306
     },
     redis: {
