44afee3f5745956b6a65738b01e49e867aaaa9d5
chensuiyi <chenbimo@gmail.com>
2023-05-28 20:53:40 +0800

    【修复 BUG】(yiapi): email字段的pattern拼写问题修正

--- packages/yiapi/config/schemaField.js
+++ packages/yiapi/config/schemaField.js
@@ -181,7 +181,7 @@ let schemaField = {
         type: 'string',
         minLength: 3,
         maxLength: 30,
-        pattren: '^[A-Za-z0-9\u4e00-\u9fa5]+@[a-zA-Z0-9_-]+(.[a-zA-Z0-9_-]+)+$'
+        pattern: '^[A-Za-z0-9\u4e00-\u9fa5]+@[a-zA-Z0-9_-]+(.[a-zA-Z0-9_-]+)+$'
     },
     // 经度
     longitude: {
