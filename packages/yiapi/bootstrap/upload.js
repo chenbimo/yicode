b4655d70ebf95100ad02d49008872bebf97c09d9
chensuiyi <chenbimo@gmail.com>
2023-06-01 17:15:12 +0800

    【修改配置】(yiapi): 文件上传的最大体积设置为100M

--- packages/yiapi/bootstrap/upload.js
+++ packages/yiapi/bootstrap/upload.js
@@ -4,7 +4,16 @@ import { appConfig } from '../config/appConfig.js';
 
 async function plugin(fastify, options) {
     await fastify.register(fastifyMultipart, {
-        attachFieldsToBody: true
+        attachFieldsToBody: true,
+        limits: {
+            fieldNameSize: 100,
+            fieldSize: 100,
+            fields: 10,
+            fileSize: 100000000, // 文件最大为100M
+            files: 1,
+            headerPairs: 2000,
+            parts: 1000
+        }
     });
 }
 
