f526c84aa493222160c3540db442ebd138eb7057
chensuiyi <chenbimo@gmail.com>
2023-06-13 20:29:55 +0800

    【功能完善】(base-vue3): 删除打印的调试环境变量

--- templates/base-vue3/src/app.js
+++ templates/base-vue3/src/app.js
@@ -1,9 +1,6 @@
 import App from '@/App.vue';
 import 'virtual:uno.css';
 
-console.log('环境变量');
-console.log(import.meta.env.VITE_NAMESPACE);
-
 const app = createApp(App);
 
 const $Pinia = Pinia.createPinia();
