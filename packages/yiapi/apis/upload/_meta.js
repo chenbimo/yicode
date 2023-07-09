1732be074f51bd2cf7ab92af964744d81a369e93
chensuiyi <chenbimo@gmail.com>
2023-07-09 15:37:42 +0800

    【功能完善】(yiapi): 补充upload接口函数的schema字段

--- packages/yiapi/apis/upload/_meta.js
+++ packages/yiapi/apis/upload/_meta.js
@@ -2,5 +2,6 @@ import { fnSchema } from '../../utils/index.js';
 import { schemaField } from '../../config/schemaField.js';
 
 export const metaConfig = {
-    name: '文件上传'
+    name: '文件上传',
+    schema: {}
 };
