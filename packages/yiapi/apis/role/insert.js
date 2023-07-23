5a59e34a3c37cddda5c44a84d95be97dc4008cae
chensuiyi <chenbimo@gmail.com>
2023-07-23 20:55:50 +0800

    【功能完善】(package/yiapi): 完善role/insert接口

--- packages/yiapi/apis/role/insert.js
+++ packages/yiapi/apis/role/insert.js
@@ -1,11 +1,12 @@
-import { fnDbInsertData, fnApiInfo } from '../../utils/index.js';
-
+// 工具函数
+import { fnDbUpdateData, fnApiInfo } from '../../utils/index.js';
+// 配置文件
 import { appConfig } from '../../config/appConfig.js';
 import { codeConfig } from '../../config/codeConfig.js';
 import { metaConfig } from './_meta.js';
-
+// 接口信息
 const apiInfo = await fnApiInfo(import.meta.url);
-
+// 传参验证
 export const apiSchema = {
     summary: `添加${metaConfig.name}`,
     tags: [apiInfo.parentDirName],
@@ -22,32 +23,31 @@ export const apiSchema = {
         required: ['name', 'code']
     }
 };
-
+// 处理函数
 export default async function (fastify, opts) {
     fastify.post(`/${apiInfo.pureFileName}`, {
         schema: apiSchema,
         handler: async function (req, res) {
             try {
-                let roleModel = fastify.mysql //
-                    .table('sys_role')
-                    .modify(function (queryBuilder) {});
+                const roleModel = fastify.mysql.table('sys_role').modify(function (queryBuilder) {});
 
-                let _result = await roleModel.clone().where('name', req.body.name).orWhere('code', req.body.code).first();
-                if (_result) {
+                const roleData = await roleModel.clone().where('name', req.body.name).orWhere('code', req.body.code).first('id');
+
+                if (roleData?.id) {
                     return {
                         ...codeConfig.INSERT_FAIL,
                         msg: '角色名称或编码已存在'
                     };
                 }
 
-                let data = {
+                const insertData = {
                     code: req.body.code,
                     name: req.body.name,
                     describe: req.body.describe,
                     menu_ids: req.body.menu_ids.join(','),
                     api_ids: req.body.api_ids.join(',')
                 };
-                let result = await roleModel.clone().insert(fnDbInsertData(data));
+                const result = await roleModel.clone().update(fnDbUpdateData(insertData));
 
                 await fastify.cacheRoleData();
 
