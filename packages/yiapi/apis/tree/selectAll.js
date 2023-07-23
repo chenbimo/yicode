c359e6d769f78c84d1a8b843e536a7f6b6cab117
chensuiyi <chenbimo@gmail.com>
2023-07-23 21:13:05 +0800

    【功能完善】(package/yiapi): 完善tree/selectAll接口

--- packages/yiapi/apis/tree/selectAll.js
+++ packages/yiapi/apis/tree/selectAll.js
@@ -1,12 +1,13 @@
+// 工具函数
 import { fnApiInfo } from '../../utils/index.js';
-
+// 配置文件
 import { appConfig } from '../../config/appConfig.js';
 import { codeConfig } from '../../config/codeConfig.js';
 import { cacheData } from '../../config/cacheData.js';
 import { metaConfig } from './_meta.js';
-
+// 接口信息
 const apiInfo = await fnApiInfo(import.meta.url);
-
+// 传参验证
 export const apiSchema = {
     summary: `查询所有${metaConfig.name}`,
     tags: [apiInfo.parentDirName],
@@ -19,21 +20,14 @@ export const apiSchema = {
         required: ['category']
     }
 };
-
+// 处理函数
 export default async function (fastify, opts) {
     fastify.post(`/${apiInfo.pureFileName}`, {
         schema: apiSchema,
         handler: async function (req, res) {
             try {
-                // TODO: 优化，不同分类的目录独立的缓存
-                let treeData = await fastify.redisGet(cacheData.tree);
-                let rows = treeData.filter((item) => item.category === req.body.category);
-                // let model = fastify.mysql //
-                //     .table('sys_tree')
-                //     .where('type', req.body.type)
-                //     .modify(function (queryBuilder) {});
-
-                // let rows = await model.clone().orderBy('sort', 'asc').select();
+                const treeData = await fastify.redisGet(cacheData.tree);
+                const rows = treeData.filter((item) => item.category === req.body.category);
 
                 return {
                     ...codeConfig.SELECT_SUCCESS,
