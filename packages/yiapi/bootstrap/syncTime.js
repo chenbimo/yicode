ecb8be9592929a7d764139ebfe2467cc265643b6
chensuiyi <chenbimo@gmail.com>
2023-07-23 21:48:29 +0800

    【增加功能】(package/yiapi): bootstrap/syncTime.js文件使用const替代let

--- packages/yiapi/bootstrap/syncTime.js
+++ packages/yiapi/bootstrap/syncTime.js
@@ -3,12 +3,12 @@ import got from 'got';
 import { differenceInSeconds, toDate } from 'date-fns';
 
 async function plugin(fastify, opts) {
-    let res = await got('http://api.m.taobao.com/rest/api3.do?api=mtop.common.getTimestamp', {
+    const res = await got('http://api.m.taobao.com/rest/api3.do?api=mtop.common.getTimestamp', {
         method: 'get'
     }).json();
-    let timestamp = res?.data?.t;
+    const timestamp = res?.data?.t;
     if (timestamp) {
-        let diffSeconds = Math.abs(differenceInSeconds(new Date(), toDate(timestamp * 1)));
+        const diffSeconds = Math.abs(differenceInSeconds(new Date(), toDate(timestamp * 1)));
         if (diffSeconds > 60) {
             fastify.log.error('与北京时间相差大于1分钟');
             process.exit();
