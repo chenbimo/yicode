f9acb041b644a79f5cdab1406f43f08d3808f831
chensuiyi <chenbimo@gmail.com>
2023-07-23 20:47:04 +0800

    【功能完善】(package/yiapi): 完善menu/insert接口

--- packages/yiapi/apis/menu/insert.js
+++ packages/yiapi/apis/menu/insert.js
@@ -1,11 +1,12 @@
+// 工具函数
 import { fnDbInsertData, fnApiInfo } from '../../utils/index.js';
-
+// 配置文件
 import { appConfig } from '../../config/appConfig.js';
 import { codeConfig } from '../../config/codeConfig.js';
 import { metaConfig } from './_meta.js';
-
+// 接口信息
 const apiInfo = await fnApiInfo(import.meta.url);
-
+// 传参验证
 export const apiSchema = {
     summary: `添加${metaConfig.name}`,
     tags: [apiInfo.parentDirName],
@@ -25,20 +26,18 @@ export const apiSchema = {
         required: ['pid', 'name', 'value']
     }
 };
-
+// 处理函数
 export default async function (fastify, opts) {
     fastify.post(`/${apiInfo.pureFileName}`, {
         schema: apiSchema,
         handler: async function (req, res) {
             try {
-                let menuModel = fastify.mysql.table('sys_menu');
+                const menuModel = fastify.mysql.table('sys_menu');
 
-                let parentData = undefined;
-
-                // 如果传了pid值
+                // 如果传了pid值，则判断父级是否存在
                 if (req.body.pid) {
-                    parentData = await menuModel.clone().where('id', req.body.pid).first();
-                    if (!parentData) {
+                    const parentData = await menuModel.clone().where('id', req.body.pid).first('id');
+                    if (!parentData?.id) {
                         return {
                             ...codeConfig.FAIL,
                             msg: '父级菜单不存在'
@@ -47,7 +46,7 @@ export default async function (fastify, opts) {
                 }
 
                 // 需要更新的数据
-                let insertData = {
+                const insertData = {
                     pid: req.body.pid,
                     name: req.body.name,
                     value: req.body.value,
