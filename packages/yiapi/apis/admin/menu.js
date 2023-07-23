9e6b557da1e4576117b617d2f1cd7d2fac38e1c8
chensuiyi <chenbimo@gmail.com>
2023-07-23 19:08:15 +0800

    【功能完善】(package/yiapi): apis/admin/menu接口完善

--- packages/yiapi/apis/admin/menu.js
+++ packages/yiapi/apis/admin/menu.js
@@ -1,11 +1,12 @@
+// 工具函数
 import { fnApiInfo } from '../../utils/index.js';
-
+// 配置文件
 import { appConfig } from '../../config/appConfig.js';
 import { codeConfig } from '../../config/codeConfig.js';
 import { metaConfig } from './_meta.js';
-
+// 接口信息
 const apiInfo = await fnApiInfo(import.meta.url);
-
+// 传参校验
 export const apiSchema = {
     tags: [apiInfo.parentDirName],
     summary: `查询${metaConfig.name}菜单权限`,
@@ -15,7 +16,7 @@ export const apiSchema = {
         properties: {}
     }
 };
-
+// 处理函数
 export default async function (fastify, opts) {
     fastify.post(`/${apiInfo.pureFileName}`, {
         schema: apiSchema,
