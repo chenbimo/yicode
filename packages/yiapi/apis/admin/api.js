ab848fe8700923e47c96dcf3c3c7abfc51ce6cd0
chensuiyi <chenbimo@gmail.com>
2023-07-23 19:09:05 +0800

    【功能完善】(package/yiapi): apis/admin/api接口增加注释

--- packages/yiapi/apis/admin/api.js
+++ packages/yiapi/apis/admin/api.js
@@ -1,9 +1,11 @@
+// 工具函数
 import { fnApiInfo } from '../../utils/index.js';
+// 配置文件
 import { codeConfig } from '../../config/codeConfig.js';
 import { metaConfig } from './_meta.js';
-
+// 接口信息
 const apiInfo = await fnApiInfo(import.meta.url);
-
+// 传参校验
 export const apiSchema = {
     summary: `查询${metaConfig.name}接口权限`,
     tags: [apiInfo.parentDirName],
@@ -13,7 +15,7 @@ export const apiSchema = {
         properties: {}
     }
 };
-
+// 处理函数
 export default async function (fastify, opts) {
     fastify.post(`/${apiInfo.pureFileName}`, {
         schema: apiSchema,
