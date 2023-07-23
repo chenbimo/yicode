96787d8154b5a0c8eda805116a76c36b3edd723d
chensuiyi <chenbimo@gmail.com>
2023-07-23 19:33:06 +0800

    【功能完善】(package/yiapi): apis/api/delete接口完善

--- packages/yiapi/apis/dict/delete.js
+++ packages/yiapi/apis/dict/delete.js
@@ -1,11 +1,12 @@
+// 工具函数
 import { fnApiInfo } from '../../utils/index.js';
-
+// 配置文件
 import { appConfig } from '../../config/appConfig.js';
 import { codeConfig } from '../../config/codeConfig.js';
 import { metaConfig } from './_meta.js';
-
+// 接口信息
 const apiInfo = await fnApiInfo(import.meta.url);
-
+// 传参校验
 export const apiSchema = {
     tags: [apiInfo.parentDirName],
     summary: `删除${metaConfig.name}`,
@@ -18,26 +19,29 @@ export const apiSchema = {
         required: ['id']
     }
 };
-
+// 处理函数
 export default async function (fastify, opts) {
     fastify.post(`/${apiInfo.pureFileName}`, {
         schema: apiSchema,
         handler: async function (req, res) {
             try {
-                let dictModel = fastify.mysql //
+                const dictModel = fastify.mysql //
                     .table('sys_dict')
                     .where({ id: req.body.id });
 
-                let dictData = await dictModel.clone().first();
+                const dictData = await dictModel.clone().first('id', 'is_system');
+                if (!dictData?.id) {
+                    return codeConfig.NO_DATA;
+                }
 
-                if (dictData.is_system === 1) {
+                if (dictData?.is_system === 1) {
                     return {
                         ...codeConfig.DELETE_FAIL,
                         msg: '默认字典，无法删除'
                     };
                 }
 
-                let result = await dictModel.clone().delete();
+                const result = await dictModel.clone().delete();
                 return {
                     ...codeConfig.DELETE_SUCCESS,
                     data: result
