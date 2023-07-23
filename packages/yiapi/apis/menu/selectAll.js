b29e2ff6ac56d281dcc15e61434547e5bdbc7c1d
chensuiyi <chenbimo@gmail.com>
2023-07-23 20:50:56 +0800

    【功能完善】(package/yiapi): 完善menu/selectAll接口

--- packages/yiapi/apis/menu/selectAll.js
+++ packages/yiapi/apis/menu/selectAll.js
@@ -1,12 +1,13 @@
+// 工具函数
 import { fnApiInfo } from '../../utils/index.js';
-
+// 配置文件
 import { appConfig } from '../../config/appConfig.js';
 import { codeConfig } from '../../config/codeConfig.js';
 import { cacheData } from '../../config/cacheData.js';
 import { metaConfig } from './_meta.js';
-
+// 接口信息
 const apiInfo = await fnApiInfo(import.meta.url);
-
+// 传参验证
 export const apiSchema = {
     summary: `查询所有${metaConfig.name}`,
     tags: [apiInfo.parentDirName],
@@ -17,13 +18,13 @@ export const apiSchema = {
         required: []
     }
 };
-
+// 处理函数
 export default async function (fastify, opts) {
     fastify.post(`/${apiInfo.pureFileName}`, {
         schema: apiSchema,
         handler: async function (req, res) {
             try {
-                let menuData = await fastify.redisGet(cacheData.menu);
+                const menuData = await fastify.redisGet(cacheData.menu);
 
                 return {
                     ...codeConfig.SELECT_SUCCESS,
