9697292f17702b8b725f12c54f5781b738eafcaa
chensuiyi <chenbimo@gmail.com>
2023-07-23 20:52:37 +0800

    【功能完善】(package/yiapi): 完善menu/update接口

--- packages/yiapi/apis/menu/update.js
+++ packages/yiapi/apis/menu/update.js
@@ -1,11 +1,12 @@
+// 工具函数
 import { fnDbUpdateData, fnApiInfo } from '../../utils/index.js';
-
+// 配置文件
 import { appConfig } from '../../config/appConfig.js';
 import { codeConfig } from '../../config/codeConfig.js';
 import { metaConfig } from './_meta.js';
-
+// 接口信息
 const apiInfo = await fnApiInfo(import.meta.url);
-
+// 传参验证
 export const apiSchema = {
     summary: `更新${metaConfig.name}`,
     tags: [apiInfo.parentDirName],
@@ -26,20 +27,18 @@ export const apiSchema = {
         required: ['id']
     }
 };
-
+// 处理函数
 export default async function (fastify, opts) {
     fastify.post(`/${apiInfo.pureFileName}`, {
         schema: apiSchema,
         handler: async function (req, res) {
             try {
-                let menuModel = fastify.mysql.table('sys_menu');
-
-                let parentData = undefined;
+                const menuModel = fastify.mysql.table('sys_menu');
 
                 // 如果传了pid值
                 if (req.body.pid) {
-                    parentData = await menuModel.clone().where('id', req.body.pid).first();
-                    if (!parentData) {
+                    const parentData = await menuModel.clone().where('id', req.body.pid).first('id');
+                    if (!parentData?.id) {
                         return {
                             ...codeConfig.FAIL,
                             msg: '父级菜单不存在'
@@ -47,8 +46,8 @@ export default async function (fastify, opts) {
                     }
                 }
 
-                let selfData = await menuModel.clone().where('id', req.body.id).first();
-                if (selfData === undefined) {
+                const selfData = await menuModel.clone().where('id', req.body.id).first('id');
+                if (!selfData?.id) {
                     return {
                         ...codeConfig.FAIL,
                         msg: '菜单不存在'
@@ -56,7 +55,7 @@ export default async function (fastify, opts) {
                 }
 
                 // 需要更新的数据
-                let updateData = {
+                const updateData = {
                     pid: req.body.pid,
                     name: req.body.name,
                     value: req.body.value,
@@ -67,7 +66,7 @@ export default async function (fastify, opts) {
                     state: req.body.state
                 };
 
-                let res = await menuModel
+                const res = await menuModel
                     //
                     .clone()
                     .where({ id: req.body.id })
