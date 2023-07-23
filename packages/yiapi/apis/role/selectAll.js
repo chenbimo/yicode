96941a47657dba97a078e1d851921a8ad7a54474
chensuiyi <chenbimo@gmail.com>
2023-07-23 20:59:28 +0800

    【功能完善】(package/yiapi): 完善role/selectAll接口

--- packages/yiapi/apis/role/selectAll.js
+++ packages/yiapi/apis/role/selectAll.js
@@ -1,11 +1,14 @@
+// 工具函数
 import { fnApiInfo } from '../../utils/index.js';
-
+// 配置文件
 import { appConfig } from '../../config/appConfig.js';
 import { codeConfig } from '../../config/codeConfig.js';
 import { metaConfig } from './_meta.js';
-
+// 接口信息
 const apiInfo = await fnApiInfo(import.meta.url);
-
+// 选择字段
+const selectKeys = fnSelectFields('./tables/role.json');
+// 传参验证
 export const apiSchema = {
     summary: `查询所有${metaConfig.name}`,
     tags: [apiInfo.parentDirName],
@@ -15,13 +18,13 @@ export const apiSchema = {
         properties: {}
     }
 };
-
+// 处理函数
 export default async function (fastify, opts) {
     fastify.post(`/${apiInfo.pureFileName}`, {
         schema: apiSchema,
         handler: async function (req, res) {
             try {
-                let roleModel = fastify.mysql //
+                const roleModel = fastify.mysql //
                     .table('sys_role')
                     .modify(function (queryBuilder) {
                         // 如果不是开发管理员查询，则排除掉开发角色
@@ -30,7 +33,7 @@ export default async function (fastify, opts) {
                         }
                     });
 
-                let rows = await roleModel.clone().select();
+                const rows = await roleModel.clone().select(selectKeys);
 
                 return {
                     ...codeConfig.SELECT_SUCCESS,
