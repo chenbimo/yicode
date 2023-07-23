9340c4fa4d435984765b913630f9e04a69986de9
chensuiyi <chenbimo@gmail.com>
2023-07-23 20:53:45 +0800

    【功能完善】(package/yiapi): 完善role/delete接口

--- packages/yiapi/apis/role/delete.js
+++ packages/yiapi/apis/role/delete.js
@@ -1,11 +1,12 @@
+// 工具函数
 import { fnApiInfo } from '../../utils/index.js';
-
+// 配置文件
 import { appConfig } from '../../config/appConfig.js';
 import { codeConfig } from '../../config/codeConfig.js';
 import { metaConfig } from './_meta.js';
-
+// 接口信息
 const apiInfo = await fnApiInfo(import.meta.url);
-
+// 传参验证
 export const apiSchema = {
     tags: [apiInfo.parentDirName],
     summary: `删除${metaConfig.name}`,
@@ -18,23 +19,20 @@ export const apiSchema = {
         required: ['id']
     }
 };
-
+// 处理函数
 export default async function (fastify, opts) {
     fastify.post(`/${apiInfo.pureFileName}`, {
         schema: apiSchema,
         handler: async function (req, res) {
             try {
-                let roleModel = fastify.mysql //
+                const roleModel = fastify.mysql //
                     .table('sys_role')
                     .where('id', req.body.id)
                     .modify(function (queryBuilder) {});
 
-                let roleData = await roleModel.clone().first();
-                if (!roleData) {
-                    return {
-                        ...codeConfig.DELETE_FAIL,
-                        msg: '角色不存在'
-                    };
+                const roleData = await roleModel.clone().first('id', 'is_system');
+                if (!roleData?.id) {
+                    return codeConfig.NO_DATA;
                 }
 
                 if (roleData.is_system === 1) {
@@ -44,7 +42,7 @@ export default async function (fastify, opts) {
                     };
                 }
 
-                let result = await roleModel.clone().delete();
+                const result = await roleModel.clone().delete();
 
                 // 生成新的权限
                 await fastify.cacheRoleData();
