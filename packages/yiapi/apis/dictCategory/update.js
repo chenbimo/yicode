673e2a51537c300b21679c19f364446bcf68728b
chensuiyi <chenbimo@gmail.com>
2023-07-23 20:34:25 +0800

    【功能完善】(package/yiapi): 完善dictCategory/update接口

--- packages/yiapi/apis/dictCategory/update.js
+++ packages/yiapi/apis/dictCategory/update.js
@@ -1,11 +1,12 @@
+// 工具函数
 import { fnDbUpdateData, fnApiInfo, fnCamelCase } from '../../utils/index.js';
-
+// 配置文件
 import { appConfig } from '../../config/appConfig.js';
 import { codeConfig } from '../../config/codeConfig.js';
 import { metaConfig } from './_meta.js';
-
+// 接口信息
 const apiInfo = await fnApiInfo(import.meta.url);
-
+// 传参验证
 export const apiSchema = {
     summary: `更新${metaConfig.name}`,
     tags: [apiInfo.parentDirName],
@@ -22,31 +23,34 @@ export const apiSchema = {
         required: ['id', 'code']
     }
 };
-
+// 处理函数
 export default async function (fastify, opts) {
     fastify.post(`/${apiInfo.pureFileName}`, {
         schema: apiSchema,
         handler: async function (req, res) {
             try {
-                let dictCategoryModel = fastify.mysql.table('sys_dict_category').modify(function (queryBuilder) {});
+                const dictCategoryModel = fastify.mysql.table('sys_dict_category').modify(function (queryBuilder) {});
 
-                let currentData = await dictCategoryModel.clone().where({ code: fnCamelCase(req.body.code) });
+                const dictCategoryData = await dictCategoryModel
+                    .clone()
+                    .where({ code: fnCamelCase(req.body.code) })
+                    .first('id');
 
-                if (currentData) {
+                if (dictCategory?.id) {
                     return {
                         ...codeConfig.INSERT_FAIL,
                         msg: '当前编号已存在'
                     };
                 }
 
-                let updateData = {
+                const updateData = {
                     code: fnCamelCase(req.body.code),
                     name: req.body.name,
                     describe: req.body.describe,
                     state: req.body.state
                 };
 
-                let result = await dictCategoryModel //
+                const result = await dictCategoryModel //
                     .clone()
                     .where({ id: req.body.id })
                     .update(fnDbUpdateData(updateData));
