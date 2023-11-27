e392d65eb3938d5d558ae02ebe33cbf751a2a722
chensuiyi <chenbimo@gmail.com>
2023-11-27 20:06:38 +0800

    重构代码(yiapi): 重构dict/detail接口

--- packages/yiapi/apis/dict/detail.js
+++ packages/yiapi/apis/dict/detail.js
@@ -1,36 +1,34 @@
 // 工具函数
-import { fnApiInfo } from '../../utils/index.js';
+import { fnRoute } from '../../utils/index.js';
 // 配置文件
-import { appConfig } from '../../config/appConfig.js';
 import { codeConfig } from '../../config/codeConfig.js';
 import { metaConfig } from './_meta.js';
-// 接口信息
-let apiInfo = await fnApiInfo(import.meta.url);
-// 传参校验
-export let apiSchema = {
-    summary: `查询${metaConfig.name}详情`,
-    tags: [apiInfo.parentDirName],
-    body: {
-        title: `查询${metaConfig.name}详情接口`,
-        type: 'object',
-        properties: {
-            code: metaConfig.schema.code
-        },
-        required: ['code']
-    }
-};
-// 处理函数
-export default async function (fastify, opts) {
-    fastify.post(`/${apiInfo.pureFileName}`, {
-        schema: apiSchema,
-        handler: async function (req, res) {
-            try {
-                let dictModel = fastify.mysql.table('sys_dict');
 
-                let result = await dictModel //
+// 处理函数
+export default async (fastify) => {
+    // 当前文件的路径，fastify 实例
+    fnRoute(import.meta.url, fastify, {
+        // 接口名称
+        apiName: '查询字典详情',
+        // 请求参数约束
+        schemaRequest: {
+            type: 'object',
+            properties: {
+                code: metaConfig.schema.code
+            },
+            required: ['code']
+        },
+        // 返回数据约束
+        schemaResponse: {},
+        // 执行函数
+        apiHandler: async (req, res) => {
+            try {
+                const dictModel = fastify.mysql.table('sys_dict');
+
+                const result = await dictModel //
                     .clone()
                     .where('code', req.body.code)
-                    .first();
+                    .selectOne();
 
                 return {
                     ...codeConfig.SELECT_SUCCESS,
@@ -42,4 +40,4 @@ export default async function (fastify, opts) {
             }
         }
     });
-}
+};
