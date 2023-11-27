269e623b1afffafc1c79368fc0a66107b178d619
chensuiyi <chenbimo@gmail.com>
2023-11-27 20:05:05 +0800

    重构代码(yiapi): 重构dict/delete接口

--- packages/yiapi/apis/dict/delete.js
+++ packages/yiapi/apis/dict/delete.js
@@ -1,35 +1,33 @@
 // 工具函数
-import { fnApiInfo } from '../../utils/index.js';
+import { fnRoute } from '../../utils/index.js';
 // 配置文件
-import { appConfig } from '../../config/appConfig.js';
 import { codeConfig } from '../../config/codeConfig.js';
 import { metaConfig } from './_meta.js';
-// 接口信息
-let apiInfo = await fnApiInfo(import.meta.url);
-// 传参校验
-export let apiSchema = {
-    tags: [apiInfo.parentDirName],
-    summary: `删除${metaConfig.name}`,
-    body: {
-        title: `删除${metaConfig.name}接口`,
-        type: 'object',
-        properties: {
-            id: metaConfig.schema.id
-        },
-        required: ['id']
-    }
-};
+
 // 处理函数
-export default async function (fastify, opts) {
-    fastify.post(`/${apiInfo.pureFileName}`, {
-        schema: apiSchema,
-        handler: async function (req, res) {
+export default async (fastify) => {
+    // 当前文件的路径，fastify 实例
+    fnRoute(import.meta.url, fastify, {
+        // 接口名称
+        apiName: '删除管理员接口',
+        // 请求参数约束
+        schemaRequest: {
+            type: 'object',
+            properties: {
+                id: metaConfig.schema.id
+            },
+            required: ['id']
+        },
+        // 返回数据约束
+        schemaResponse: {},
+        // 执行函数
+        apiHandler: async (req, res) => {
             try {
-                let dictModel = fastify.mysql //
+                const dictModel = fastify.mysql //
                     .table('sys_dict')
                     .where({ id: req.body.id });
 
-                let dictData = await dictModel.clone().first('id', 'is_system');
+                const dictData = await dictModel.clone().selectOne('id', 'is_system');
                 if (!dictData?.id) {
                     return codeConfig.NO_DATA;
                 }
@@ -41,7 +39,7 @@ export default async function (fastify, opts) {
                     };
                 }
 
-                let result = await dictModel.clone().delete();
+                const result = await dictModel.clone().deleteData();
                 return {
                     ...codeConfig.DELETE_SUCCESS,
                     data: result
@@ -52,4 +50,4 @@ export default async function (fastify, opts) {
             }
         }
     });
-}
+};
