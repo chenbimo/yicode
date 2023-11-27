b0902232aeed34df36b994ca2125e15fb912b15d
chensuiyi <chenbimo@gmail.com>
2023-11-27 21:31:45 +0800

    重构代码(yiapi): 重构menu/insert接口

--- packages/yiapi/apis/menu/insert.js
+++ packages/yiapi/apis/menu/insert.js
@@ -1,42 +1,40 @@
 // 工具函数
-import { fnDbInsertData, fnApiInfo } from '../../utils/index.js';
+import { fnRoute } from '../../utils/index.js';
 // 配置文件
-import { appConfig } from '../../config/appConfig.js';
 import { codeConfig } from '../../config/codeConfig.js';
 import { metaConfig } from './_meta.js';
-// 接口信息
-let apiInfo = await fnApiInfo(import.meta.url);
-// 传参验证
-export let apiSchema = {
-    summary: `添加${metaConfig.name}`,
-    tags: [apiInfo.parentDirName],
-    body: {
-        title: `添加${metaConfig.name}接口`,
-        type: 'object',
-        properties: {
-            pid: metaConfig.schema.pid,
-            name: metaConfig.schema.name,
-            value: metaConfig.schema.value,
-            icon: metaConfig.schema.icon,
-            sort: metaConfig.schema.sort,
-            state: metaConfig.schema.state,
-            describe: metaConfig.schema.describe,
-            is_open: metaConfig.schema.is_open
-        },
-        required: ['pid', 'name', 'value']
-    }
-};
-// 处理函数
-export default async function (fastify, opts) {
-    fastify.post(`/${apiInfo.pureFileName}`, {
-        schema: apiSchema,
-        handler: async function (req, res) {
-            try {
-                let menuModel = fastify.mysql.table('sys_menu');
 
-                // 如果传了pid值，则判断父级是否存在
+// 处理函数
+export default async (fastify) => {
+    // 当前文件的路径，fastify 实例
+    fnRoute(import.meta.url, fastify, {
+        // 接口名称
+        apiName: '添加菜单',
+        // 请求参数约束
+        schemaRequest: {
+            type: 'object',
+            properties: {
+                pid: metaConfig.schema.pid,
+                name: metaConfig.schema.name,
+                value: metaConfig.schema.value,
+                icon: metaConfig.schema.icon,
+                sort: metaConfig.schema.sort,
+                state: metaConfig.schema.state,
+                describe: metaConfig.schema.describe,
+                is_open: metaConfig.schema.is_open
+            },
+            required: ['pid', 'name', 'value']
+        },
+        // 返回数据约束
+        schemaResponse: {},
+        // 执行函数
+        apiHandler: async (req, res) => {
+            try {
+                const menuModel = fastify.mysql.table('sys_menu');
+
+                // 如果传了 pid 值，则判断父级是否存在
                 if (req.body.pid) {
-                    let parentData = await menuModel.clone().where('id', req.body.pid).first('id');
+                    let parentData = await menuModel.clone().where('id', req.body.pid).selectOne('id');
                     if (!parentData?.id) {
                         return {
                             ...codeConfig.FAIL,
@@ -45,8 +43,7 @@ export default async function (fastify, opts) {
                     }
                 }
 
-                // 需要更新的数据
-                let insertData = {
+                await menuModel.clone().insertData({
                     pid: req.body.pid,
                     name: req.body.name,
                     value: req.body.value,
@@ -55,9 +52,7 @@ export default async function (fastify, opts) {
                     is_open: req.body.is_open,
                     describe: req.body.describe,
                     state: req.body.state
-                };
-
-                await menuModel.clone().insert(fnDbInsertData(insertData));
+                });
 
                 await fastify.cacheTreeData();
                 return codeConfig.INSERT_SUCCESS;
@@ -67,4 +62,4 @@ export default async function (fastify, opts) {
             }
         }
     });
-}
+};
