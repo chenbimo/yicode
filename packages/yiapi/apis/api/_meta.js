28505dffb87509de632bc153b3019847338cf88e
chensuiyi <chenbimo@gmail.com>
2023-11-27 19:31:01 +0800

    调整配置(yiapi): 配置api目录meta文件

--- packages/yiapi/apis/api/_meta.js
+++ packages/yiapi/apis/api/_meta.js
@@ -1,10 +1,7 @@
-import { fnSchema } from '../../utils/index.js';
+import { fnSchema, fnMeta } from '../../utils/index.js';
 import { schemaField } from '../../config/schemaField.js';
 
-export let metaConfig = {
+export const metaConfig = fnMeta(import.meta.url, {
     name: '接口',
-    schema: {
-        page: fnSchema(schemaField.page, '第几页'),
-        limit: fnSchema(schemaField.limit, '每页多少条')
-    }
-};
+    schema: {}
+});
