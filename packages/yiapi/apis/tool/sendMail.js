faa484aedea2e1c523fb1de9a612666d11ce772a
chensuiyi <chenbimo@gmail.com>
2023-11-27 21:55:24 +0800

    重构代码(yiapi): 重构tool/sendEmail接口

--- packages/yiapi/apis/tool/sendMail.js
+++ packages/yiapi/apis/tool/sendMail.js
@@ -1,63 +1,61 @@
 // 工具函数
-import { fnApiInfo, fnRandom6Number, fnDbInsertData } from '../../utils/index.js';
+import { fnRoute, fnRandom6Number } from '../../utils/index.js';
 // 配置文件
-import { appConfig } from '../../config/appConfig.js';
 import { codeConfig } from '../../config/codeConfig.js';
 import { metaConfig } from './_meta.js';
-// 接口信息
-let apiInfo = await fnApiInfo(import.meta.url);
-// 传参验证
-export let apiSchema = {
-    summary: `发送邮箱注册验证码`,
-    tags: [apiInfo.parentDirName],
-    body: {
-        oneOf: [
-            {
-                title: '发送验证码邮件',
-                type: 'object',
-                properties: {
-                    to_email: metaConfig.schema.to_email,
-                    subject: metaConfig.schema.subject,
-                    verify_name: metaConfig.schema.verify_name
-                },
-                required: ['to_email', 'subject', 'verify_name']
-            },
-            {
-                title: '发送普通邮件',
-                type: 'object',
-                properties: {
-                    to_email: metaConfig.schema.to_email,
-                    subject: metaConfig.schema.subject,
-                    content: metaConfig.schema.content
-                },
-                required: ['to_email', 'subject', 'content']
-            }
-        ]
-    }
-};
+
 // 处理函数
-export default async function (fastify, opts) {
-    fastify.post(`/${apiInfo.pureFileName}`, {
-        schema: apiSchema,
-        handler: async function (req, res) {
+export default async (fastify) => {
+    // 当前文件的路径，fastify 实例
+    fnRoute(import.meta.url, fastify, {
+        // 接口名称
+        apiName: '发送邮件',
+        // 请求参数约束
+        schemaRequest: {
+            oneOf: [
+                {
+                    title: '发送验证码邮件',
+                    type: 'object',
+                    properties: {
+                        to_email: metaConfig.schema.to_email,
+                        subject: metaConfig.schema.subject,
+                        verify_name: metaConfig.schema.verify_name
+                    },
+                    required: ['to_email', 'subject', 'verify_name']
+                },
+                {
+                    title: '发送普通邮件',
+                    type: 'object',
+                    properties: {
+                        to_email: metaConfig.schema.to_email,
+                        subject: metaConfig.schema.subject,
+                        content: metaConfig.schema.content
+                    },
+                    required: ['to_email', 'subject', 'content']
+                }
+            ]
+        },
+        // 返回数据约束
+        schemaResponse: {},
+        // 执行函数
+        apiHandler: async (req, res) => {
             try {
-                let mailLogModel = fastify.mysql.table('sys_mail_log');
+                const mailLogModel = fastify.mysql.table('sys_mail_log');
                 // 普通发送
                 if (req.body.content) {
-                    let result = await fastify.sendEmail({
+                    const result = await fastify.sendEmail({
                         to: req.body.to_email,
                         subject: req.body.subject,
                         text: req.body.content
                     });
-                    let insertData = {
+                    await mailLogModel.clone().insertData({
                         login_email: appConfig.mail.user,
                         from_name: appConfig.mail.from_name,
                         from_email: appConfig.mail.from_email,
                         to_email: req.body.to_email,
                         email_type: 'common',
                         text_content: req.body.content
-                    };
-                    await mailLogModel.clone().insert(fnDbInsertData(insertData));
+                    });
                     return {
                         ...codeConfig.SUCCESS,
                         msg: '邮件已发送',
@@ -68,35 +66,34 @@ export default async function (fastify, opts) {
                 // 发送验证码
                 if (req.body.verify_name) {
                     // 如果已经发送过
-                    let existsVerifyCode = await fastify.redisGet(`${req.body.verify_name}:${req.body.to_email}`);
+                    const existsVerifyCode = await fastify.redisGet(`${req.body.verify_name}:${req.body.to_email}`);
                     if (existsVerifyCode) {
                         return {
                             ...codeConfig.SUCCESS,
-                            msg: '邮箱验证码已发送（5分钟有效）',
+                            msg: '邮箱验证码已发送（5 分钟有效）',
                             from: 'cache'
                         };
                     }
 
                     // 如果没有发送过
-                    let cacheVerifyCode = fnRandom6Number();
+                    const cacheVerifyCode = fnRandom6Number();
                     await fastify.redisSet(`${req.body.verify_name}:${req.body.to_email}`, cacheVerifyCode, 60 * 5);
-                    let result = await fastify.sendEmail({
+                    const result = await fastify.sendEmail({
                         to: req.body.to_email,
                         subject: req.body.subject,
                         text: req.body.subject + '：' + cacheVerifyCode
                     });
-                    let insertData = {
+                    await mailLogModel.clone().insertData({
                         login_email: appConfig.mail.user,
                         from_name: appConfig.mail.from_name,
                         from_email: appConfig.mail.from_email,
                         to_email: req.body.to_email,
                         email_type: 'verify',
                         text_content: '******'
-                    };
-                    await mailLogModel.clone().insert(fnDbInsertData(insertData));
+                    });
                     return {
                         ...codeConfig.SUCCESS,
-                        msg: '邮箱验证码已发送（5分钟有效）',
+                        msg: '邮箱验证码已发送（5 分钟有效）',
                         from: 'new'
                     };
                 }
@@ -106,4 +103,4 @@ export default async function (fastify, opts) {
             }
         }
     });
-}
+};
