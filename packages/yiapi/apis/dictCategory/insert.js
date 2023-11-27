5a6b847a4d8352dbf44164433b206c44bd5a619e
chensuiyi <chenbimo@gmail.com>
2023-11-27 20:36:27 +0800

    重构代码(yiapi): 重构dictCategory/insert接口

--- packages/yiapi/apis/dictCategory/insert.js
+++ packages/yiapi/apis/dictCategory/insert.js
@@ -1,38 +1,36 @@
 // 工具函数
-import { fnDbInsertData, fnApiInfo, fnCamelCase } from '../../utils/index.js';
+import { fnRoute, fnCamelCase } from '../../utils/index.js';
 // 配置文件
-import { appConfig } from '../../config/appConfig.js';
 import { codeConfig } from '../../config/codeConfig.js';
 import { metaConfig } from './_meta.js';
-// 接口信息
-let apiInfo = await fnApiInfo(import.meta.url);
-// 传参校验
-export let apiSchema = {
-    summary: `添加${metaConfig.name}`,
-    tags: [apiInfo.parentDirName],
-    body: {
-        title: `添加${metaConfig.name}接口`,
-        type: 'object',
-        properties: {
-            code: metaConfig.schema.code,
-            name: metaConfig.schema.name,
-            describe: metaConfig.schema.describe
-        },
-        required: ['code', 'name']
-    }
-};
-// 处理函数
-export default async function (fastify, opts) {
-    fastify.post(`/${apiInfo.pureFileName}`, {
-        schema: apiSchema,
-        handler: async function (req, res) {
-            try {
-                let dictCategoryModel = fastify.mysql.table('sys_dict_category');
 
-                let dictCategoryData = await dictCategoryModel
+// 处理函数
+export default async (fastify) => {
+    // 当前文件的路径，fastify 实例
+    fnRoute(import.meta.url, fastify, {
+        // 接口名称
+        apiName: '添加管理员',
+        // 请求参数约束
+        schemaRequest: {
+            type: 'object',
+            properties: {
+                code: metaConfig.schema.code,
+                name: metaConfig.schema.name,
+                describe: metaConfig.schema.describe
+            },
+            required: ['code', 'name']
+        },
+        // 返回数据约束
+        schemaResponse: {},
+        // 执行函数
+        apiHandler: async (req, res) => {
+            try {
+                const dictCategoryModel = fastify.mysql.table('sys_dict_category');
+
+                const dictCategoryData = await dictCategoryModel
                     .clone()
                     .where({ code: fnCamelCase(req.body.code) })
-                    .first('id');
+                    .selectOne('id');
 
                 if (dictCategoryData?.id) {
                     return {
@@ -41,13 +39,11 @@ export default async function (fastify, opts) {
                     };
                 }
 
-                let insertData = {
+                const result = await dictCategoryModel.insertData({
                     code: fnCamelCase(req.body.code),
                     name: req.body.name,
                     describe: req.body.describe
-                };
-
-                let result = await dictCategoryModel.insert(fnDbInsertData(insertData));
+                });
 
                 return {
                     ...codeConfig.INSERT_SUCCESS,
@@ -59,4 +55,4 @@ export default async function (fastify, opts) {
             }
         }
     });
-}
+};
