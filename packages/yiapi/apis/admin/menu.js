2aaf5675923a39e58eb6d1a50a35f276c5c92ae0
chensuiyi <chenbimo@gmail.com>
2023-11-27 14:02:02 +0800

    重构代码(yiapi): 重构管理登录菜单接口

--- packages/yiapi/apis/admin/menu.js
+++ packages/yiapi/apis/admin/menu.js
@@ -1,32 +1,30 @@
 // 工具函数
-import { fnApiInfo } from '../../utils/index.js';
+import { fnRoute } from '../../utils/index.js';
 // 配置文件
-import { appConfig } from '../../config/appConfig.js';
 import { codeConfig } from '../../config/codeConfig.js';
 import { metaConfig } from './_meta.js';
-// 接口信息
-let apiInfo = await fnApiInfo(import.meta.url);
-// 传参校验
-export let apiSchema = {
-    tags: [apiInfo.parentDirName],
-    summary: `查询${metaConfig.name}菜单权限`,
-    body: {
-        title: `查询${metaConfig.name}菜单权限接口`,
-        type: 'object',
-        properties: {}
-    }
-};
+
 // 处理函数
-export default async function (fastify, opts) {
-    fastify.post(`/${apiInfo.pureFileName}`, {
-        schema: apiSchema,
-        handler: async function (req, res) {
+export default async (fastify) => {
+    // 当前文件的路径，fastify 实例
+    fnRoute(import.meta.url, fastify, {
+        // 接口名称
+        apiName: '查询管理员菜单权限',
+        // 请求参数约束
+        schemaRequest: {
+            type: 'object',
+            properties: {}
+        },
+        // 返回数据约束
+        schemaResponse: {},
+        // 执行函数
+        apiHandler: async (req, res) => {
             try {
-                let userMenus = await fastify.getUserMenus(req.session);
+                let result = await fastify.getUserMenus(req.session);
                 return {
                     ...codeConfig.SELECT_SUCCESS,
                     data: {
-                        rows: userMenus
+                        rows: result
                     }
                 };
             } catch (err) {
@@ -35,4 +33,4 @@ export default async function (fastify, opts) {
             }
         }
     });
-}
+};
