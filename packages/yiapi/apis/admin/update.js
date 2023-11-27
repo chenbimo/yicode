7854916b5afb558075ac075992e99ddd93081889
chensuiyi <chenbimo@gmail.com>
2023-11-27 14:02:22 +0800

    重构代码(yiapi): 重构更新管理接口

--- packages/yiapi/apis/admin/update.js
+++ packages/yiapi/apis/admin/update.js
@@ -1,37 +1,35 @@
 // 工具函数
-import { fnTimestamp, fnDbUpdateData, fnApiInfo, fnMD5 } from '../../utils/index.js';
+import { fnRoute, fnMD5 } from '../../utils/index.js';
 // 配置文件
-import { appConfig } from '../../config/appConfig.js';
 import { codeConfig } from '../../config/codeConfig.js';
 import { metaConfig } from './_meta.js';
-// 接口信息
-let apiInfo = await fnApiInfo(import.meta.url);
-// 传参校验
-export let apiSchema = {
-    tags: [apiInfo.parentDirName],
-    summary: `更新${metaConfig.name}`,
-    body: {
-        title: `更新${metaConfig.name}接口`,
-        type: 'object',
-        properties: {
-            id: metaConfig.schema.id,
-            password: metaConfig.schema.password,
-            nickname: metaConfig.schema.nickname,
-            role_codes: metaConfig.schema.role_codes
-        },
-        required: ['id']
-    }
-};
+
 // 处理函数
-export default async function (fastify, opts) {
-    fastify.post(`/${apiInfo.pureFileName}`, {
-        schema: apiSchema,
-        handler: async function (req, res) {
+export default async (fastify) => {
+    // 当前文件的路径，fastify 实例
+    fnRoute(import.meta.url, fastify, {
+        // 接口名称
+        apiName: '添加管理员',
+        // 请求参数约束
+        schemaRequest: {
+            type: 'object',
+            properties: {
+                id: metaConfig.schema.id,
+                password: metaConfig.schema.password,
+                nickname: metaConfig.schema.nickname,
+                role_codes: metaConfig.schema.role_codes
+            },
+            required: ['id']
+        },
+        // 返回数据约束
+        schemaResponse: {},
+        // 执行函数
+        apiHandler: async (req, res) => {
             try {
                 let adminModel = fastify.mysql //
                     .table('sys_admin')
                     .where({ id: req.body.id })
-                    .modify(function (queryBuilder) {});
+                    .modify(function (qb) {});
 
                 let updateData = {
                     password: fnMD5(req.body.password),
@@ -39,7 +37,7 @@ export default async function (fastify, opts) {
                     role_codes: req.body.role_codes
                 };
 
-                await adminModel.update(fnDbUpdateData(updateData));
+                await adminModel.clone().updateData(updateData);
 
                 return codeConfig.UPDATE_SUCCESS;
             } catch (err) {
@@ -48,4 +46,4 @@ export default async function (fastify, opts) {
             }
         }
     });
-}
+};
