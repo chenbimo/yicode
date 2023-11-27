c0acc243f869e34b7fa8177f41bf43441ac7a304
chensuiyi <chenbimo@gmail.com>
2023-11-27 22:28:27 +0800

    重构代码(yiapi): 重构user/select接口

--- packages/yiapi/apis/user/select.js
+++ packages/yiapi/apis/user/select.js
@@ -1,53 +1,49 @@
 // 工具函数
-import { fnApiInfo, fnPageOffset, fnSelectFields } from '../../utils/index.js';
+import { fnRoute, fnSelectFields } from '../../utils/index.js';
 // 配置文件
-import { appConfig } from '../../config/appConfig.js';
 import { codeConfig } from '../../config/codeConfig.js';
 import { metaConfig } from './_meta.js';
 // 接口信息
-let apiInfo = await fnApiInfo(import.meta.url);
-// 选择字段
-let selectKeys = fnSelectFields('./tables/user.json', 'core', ['password']);
-// 传参验证
-export let apiSchema = {
-    tags: [apiInfo.parentDirName],
-    summary: `查询${metaConfig.name}`,
-    body: {
-        title: `查询${metaConfig.name}接口`,
-        type: 'object',
-        properties: {
-            page: metaConfig.schema.page,
-            limit: metaConfig.schema.limit
-        }
-    }
-};
+
 // 处理函数
-export default async function (fastify, opts) {
-    fastify.post(`/${apiInfo.pureFileName}`, {
-        schema: apiSchema,
-        handler: async function (req, res) {
+export default async (fastify) => {
+    // 当前文件的路径，fastify 实例
+    fnRoute(import.meta.url, fastify, {
+        // 接口名称
+        apiName: '查询用户列表',
+        // 请求参数约束
+        schemaRequest: {
+            type: 'object',
+            properties: {
+                page: metaConfig.schema.page,
+                limit: metaConfig.schema.limit
+            },
+            required: []
+        },
+        // 返回数据约束
+        schemaResponse: {},
+        // 执行函数
+        apiHandler: async (req, res) => {
             try {
-                let userModel = fastify.mysql //
+                const userModel = fastify.mysql //
                     .table('sys_user')
-                    .modify(function (queryBuilder) {
+                    .modify(function (qb) {
                         if (req.body.state !== undefined) {
-                            queryBuilder.where('state', req.body.state);
+                            qb.where('state', req.body.state);
                         }
                     });
 
-                let { total } = await userModel.clone().count('id', { as: 'total' }).first();
-                let rows = await userModel
+                const { totalCount } = await userModel.clone().selectCount();
+                const rows = await userModel
                     //
                     .clone()
                     .orderBy('created_at', 'desc')
-                    .offset(fnPageOffset(req.body.page, req.body.limit))
-                    .limit(req.body.limit)
-                    .select(selectKeys);
+                    .select(req.body.page, req.body.limit, fnSelectFields('./tables/user.json', 'core', ['password']));
 
                 return {
                     ...codeConfig.SELECT_SUCCESS,
                     data: {
-                        total: total,
+                        total: totalCount,
                         rows: rows,
                         page: req.body.page,
                         limit: req.body.limit
@@ -59,4 +55,4 @@ export default async function (fastify, opts) {
             }
         }
     });
-}
+};
