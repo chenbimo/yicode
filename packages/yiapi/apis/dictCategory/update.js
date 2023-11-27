3dde68d9e1066669c6e4c42d6826072bea9d1606
chensuiyi <chenbimo@gmail.com>
2023-11-27 20:43:39 +0800

    重构代码(yiapi): 重构dictCategory/update接口

--- packages/yiapi/apis/dictCategory/update.js
+++ packages/yiapi/apis/dictCategory/update.js
@@ -1,37 +1,36 @@
 // 工具函数
-import { fnDbUpdateData, fnApiInfo, fnCamelCase } from '../../utils/index.js';
+import { fnRoute, fnCamelCase } from '../../utils/index.js';
 // 配置文件
-import { appConfig } from '../../config/appConfig.js';
 import { codeConfig } from '../../config/codeConfig.js';
 import { metaConfig } from './_meta.js';
 // 接口信息
-let apiInfo = await fnApiInfo(import.meta.url);
-// 传参验证
-export let apiSchema = {
-    summary: `更新${metaConfig.name}`,
-    tags: [apiInfo.parentDirName],
-    body: {
-        title: `更新${metaConfig.name}接口`,
-        type: 'object',
-        properties: {
-            id: metaConfig.schema.id,
-            code: metaConfig.schema.code,
-            name: metaConfig.schema.name,
-            describe: metaConfig.schema.describe,
-            state: metaConfig.schema.state
-        },
-        required: ['id', 'code']
-    }
-};
-// 处理函数
-export default async function (fastify, opts) {
-    fastify.post(`/${apiInfo.pureFileName}`, {
-        schema: apiSchema,
-        handler: async function (req, res) {
-            try {
-                let dictCategoryModel = fastify.mysql.table('sys_dict_category').modify(function (queryBuilder) {});
 
-                let dictCategoryData = await dictCategoryModel
+// 处理函数
+export default async (fastify) => {
+    // 当前文件的路径，fastify 实例
+    fnRoute(import.meta.url, fastify, {
+        // 接口名称
+        apiName: '更新字典目录',
+        // 请求参数约束
+        schemaRequest: {
+            type: 'object',
+            properties: {
+                id: metaConfig.schema.id,
+                code: metaConfig.schema.code,
+                name: metaConfig.schema.name,
+                describe: metaConfig.schema.describe,
+                state: metaConfig.schema.state
+            },
+            required: ['id', 'code']
+        },
+        // 返回数据约束
+        schemaResponse: {},
+        // 执行函数
+        apiHandler: async (req, res) => {
+            try {
+                const dictCategoryModel = fastify.mysql.table('sys_dict_category').modify(function (qb) {});
+
+                const dictCategoryData = await dictCategoryModel
                     .clone()
                     .where({ code: fnCamelCase(req.body.code) })
                     .first('id');
@@ -43,17 +42,15 @@ export default async function (fastify, opts) {
                     };
                 }
 
-                let updateData = {
-                    code: fnCamelCase(req.body.code),
-                    name: req.body.name,
-                    describe: req.body.describe,
-                    state: req.body.state
-                };
-
-                let result = await dictCategoryModel //
+                const result = await dictCategoryModel //
                     .clone()
                     .where({ id: req.body.id })
-                    .update(fnDbUpdateData(updateData));
+                    .updateData({
+                        code: fnCamelCase(req.body.code),
+                        name: req.body.name,
+                        describe: req.body.describe,
+                        state: req.body.state
+                    });
 
                 return codeConfig.UPDATE_SUCCESS;
             } catch (err) {
@@ -62,4 +59,4 @@ export default async function (fastify, opts) {
             }
         }
     });
-}
+};
