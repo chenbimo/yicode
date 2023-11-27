c1d46432bd7fbd6aa52dd72e70472a39a0d02ed0
chensuiyi <chenbimo@gmail.com>
2023-11-27 20:46:30 +0800

    重构代码(yiapi): 重构loginLog/delete接口

--- packages/yiapi/apis/loginLog/delete.js
+++ packages/yiapi/apis/loginLog/delete.js
@@ -1,33 +1,31 @@
 // 工具函数
-import { fnApiInfo } from '../../utils/index.js';
+import { fnRoute } from '../../utils/index.js';
 // 配置文件
-import { appConfig } from '../../config/appConfig.js';
 import { codeConfig } from '../../config/codeConfig.js';
 import { metaConfig } from './_meta.js';
-// 接口信息
-let apiInfo = await fnApiInfo(import.meta.url);
-// 传参验证
-export let apiSchema = {
-    tags: [apiInfo.parentDirName],
-    summary: `删除${metaConfig.name}`,
-    body: {
-        title: `删除${metaConfig.name}接口`,
-        type: 'object',
-        properties: {
-            id: metaConfig.schema.id
-        },
-        required: ['id']
-    }
-};
-// 处理函数
-export default async function (fastify, opts) {
-    fastify.post(`/${apiInfo.pureFileName}`, {
-        schema: apiSchema,
-        handler: async function (req, res) {
-            try {
-                let loginLogModel = fastify.mysql.table('sys_login_log');
 
-                let result = await loginLogModel.clone().where({ id: req.body.id }).delete();
+// 处理函数
+export default async (fastify) => {
+    // 当前文件的路径，fastify 实例
+    fnRoute(import.meta.url, fastify, {
+        // 接口名称
+        apiName: '删除登录日志',
+        // 请求参数约束
+        schemaRequest: {
+            type: 'object',
+            properties: {
+                id: metaConfig.schema.id
+            },
+            required: ['id']
+        },
+        // 返回数据约束
+        schemaResponse: {},
+        // 执行函数
+        apiHandler: async (req, res) => {
+            try {
+                const loginLogModel = fastify.mysql.table('sys_login_log');
+
+                const result = await loginLogModel.clone().where({ id: req.body.id }).deleteData();
 
                 return {
                     ...codeConfig.DELETE_SUCCESS,
@@ -39,4 +37,4 @@ export default async function (fastify, opts) {
             }
         }
     });
-}
+};
