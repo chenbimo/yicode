4c39639157c94b7322cf51060e27b97a61bb9e34
chensuiyi <chenbimo@gmail.com>
2023-11-27 19:34:12 +0800

    调整配置(yiapi): 配置menu目录meta文件

--- packages/yiapi/apis/menu/_meta.js
+++ packages/yiapi/apis/menu/_meta.js
@@ -1,14 +1,10 @@
-import { fnSchema } from '../../utils/index.js';
+import { fnSchema, fnMeta } from '../../utils/index.js';
 import { schemaField } from '../../config/schemaField.js';
 
-export let metaConfig = {
+export let metaConfig = fnMeta(import.meta.url, {
     name: '菜单',
     schema: {
-        id: fnSchema(schemaField.id, '唯一ID'),
-        page: fnSchema(schemaField.page, '第几页'),
-        limit: fnSchema(schemaField.limit, '每页多少条'),
-        state: fnSchema(schemaField.state, '是否启用'),
-        pid: fnSchema(schemaField.pid, '父级目录ID'),
+        pid: fnSchema(schemaField.pid, '父级目录 ID'),
         name: fnSchema(null, '目录名称', 'string', 1, 30),
         value: fnSchema(schemaField.route, '菜单路由'),
         icon: fnSchema(schemaField.image, '目录图标'),
@@ -17,4 +13,4 @@ export let metaConfig = {
         describe: fnSchema(schemaField.describe, '目录描述'),
         is_open: fnSchema(schemaField.boolEnum, '是否公开')
     }
-};
+});
