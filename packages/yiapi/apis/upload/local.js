421158d3615099f89981ed1ab7d68b8f353e4f1b
chensuiyi <chenbimo@gmail.com>
2023-11-27 22:26:21 +0800

    重构代码(yiapi): 重构upload/local接口

--- packages/yiapi/apis/upload/local.js
+++ packages/yiapi/apis/upload/local.js
@@ -1,72 +1,71 @@
 // 内部模块
-import { format } from 'date-fns';
 import { resolve } from 'node:path';
 import { writeFileSync } from 'node:fs';
 // 外部模块
 import { ensureDirSync, ensureFileSync } from 'fs-extra';
+import { format } from 'date-fns';
 // 工具函数
-import { fnApiInfo, fnUUID } from '../../utils/index.js';
+import { fnRoute, fnUUID } from '../../utils/index.js';
 // 配置文件
 import { appConfig } from '../../config/appConfig.js';
 import { sysConfig } from '../../config/sysConfig.js';
 import { codeConfig } from '../../config/codeConfig.js';
 import { metaConfig } from './_meta.js';
-// 接口信息
-let apiInfo = await fnApiInfo(import.meta.url);
-// 传参验证
-export let apiSchema = {
-    summary: `文件上传到本地`,
-    tags: [apiInfo.parentDirName],
-    body: {
-        title: '文件上传到本地接口',
-        type: 'object',
-        properties: {
-            file: {
-                type: 'object',
-                title: '文件',
-                properties: {
-                    type: {
-                        title: '文件内容',
-                        type: 'string',
-                        enum: ['file']
+
+// 处理函数
+export default async (fastify) => {
+    // 当前文件的路径，fastify 实例
+    fnRoute(import.meta.url, fastify, {
+        // 接口名称
+        apiName: '文件上传到本地',
+        // 请求参数约束
+        schemaRequest: {
+            type: 'object',
+            properties: {
+                file: {
+                    type: 'object',
+                    title: '文件',
+                    properties: {
+                        type: {
+                            title: '文件内容',
+                            type: 'string',
+                            enum: ['file']
+                        }
+                    }
+                },
+                dir: {
+                    type: 'object',
+                    title: '目录',
+                    properties: {
+                        value: {
+                            title: '目录名称',
+                            type: 'string',
+                            pattern: '^[a-z][a-z0-9_-]*$'
+                        }
                     }
                 }
             },
-            dir: {
-                type: 'object',
-                title: '目录',
-                properties: {
-                    value: {
-                        title: '目录名称',
-                        type: 'string',
-                        pattern: '^[a-z][a-z0-9_-]*$'
-                    }
-                }
-            }
+            required: ['file']
         },
-        required: ['file']
-    }
-};
-// 处理函数
-export default async function (fastify, opts) {
-    fastify.post(`/${apiInfo.pureFileName}`, {
-        schema: apiSchema,
-        handler: async function (req, res) {
+        // 返回数据约束
+        schemaResponse: {},
+        // 执行函数
+        apiHandler: async (req, res) => {
             try {
-                let data = req.body.file;
+                const data = req.body.file;
 
-                let extname = data.mimetype.split('/')[1];
+                const extname = data.mimetype.split('/')[1];
 
-                let buffer = await data.toBuffer();
+                const buffer = await data.toBuffer();
 
-                let year = format(new Date(), 'yyyy');
-                let month = format(new Date(), 'MM');
+                const year = format(new Date(), 'yyyy');
+                const month = format(new Date(), 'MM');
 
-                let dir = req.body.dir.value ? `${req.body.dir.value}/${year}-${month}` : `${year}-${month}`;
-                let name = `${fnUUID()}.${extname}`;
-                let path = `${dir}/${name}`;
+                const dir = req.body.dir.value ? `${req.body.dir.value}/${year}-${month}` : `${year}-${month}`;
+                const name = `${fnUUID()}.${extname}`;
+                const path = `${dir}/${name}`;
 
-                let localDir = resolve(sysConfig.appDir, appConfig.upload.dir || 'public', dir);
+                const localDir = resolve(sysConfig.appDir, appConfig.upload.dir || 'public', dir);
                 await ensureDirSync(localDir);
                 await writeFileSync(`${localDir}/${name}`, buffer);
 
@@ -81,4 +80,4 @@ export default async function (fastify, opts) {
             }
         }
     });
-}
+};
