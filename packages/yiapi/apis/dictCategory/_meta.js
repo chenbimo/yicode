7fd18f3f78eaabc6278e3c7958134abc16cd42f4
chensuiyi <chenbimo@gmail.com>
2023-11-27 19:32:17 +0800

    调整配置(yiapi): 配置dictCategory目录meta文件

--- packages/yiapi/apis/dictCategory/_meta.js
+++ packages/yiapi/apis/dictCategory/_meta.js
@@ -1,15 +1,11 @@
-import { fnSchema } from '../../utils/index.js';
+import { fnSchema, fnMeta } from '../../utils/index.js';
 import { schemaField } from '../../config/schemaField.js';
 
-export let metaConfig = {
+export let metaConfig = fnMeta(import.meta.url, {
     name: '字典分类',
     schema: {
-        id: fnSchema(schemaField.id, '唯一ID'),
-        page: fnSchema(schemaField.page, '第几页'),
-        limit: fnSchema(schemaField.limit, '每页多少条'),
-        state: fnSchema(schemaField.state, '是否启用'),
         code: fnSchema(schemaField.Aa123_d, '字典编码'),
         name: fnSchema(null, '字典分类名称', 'string', 1, 20),
         describe: fnSchema(null, '字典分类描述', 'string', 0, 300)
     }
-};
+});
