c6289880adfe136e5dae640c752afe137942840f
chensuiyi <chenbimo@gmail.com>
2023-11-27 21:23:13 +0800

    重构代码(yiapi): 重构emailLog/select接口

--- packages/yiapi/apis/mailLog/select.js
+++ packages/yiapi/apis/mailLog/select.js
@@ -1,57 +1,51 @@
 // 工具函数
-import { fnApiInfo, fnPageOffset, fnSelectFields } from '../../utils/index.js';
+import { fnRoute, fnSelectFields } from '../../utils/index.js';
 // 配置文件
-import { appConfig } from '../../config/appConfig.js';
 import { codeConfig } from '../../config/codeConfig.js';
 import { metaConfig } from './_meta.js';
-// 接口信息
-let apiInfo = await fnApiInfo(import.meta.url);
-// 选择字段
-let selectKeys = fnSelectFields('./tables/mailLog.json');
-// 传参验证
-export let apiSchema = {
-    summary: `查询${metaConfig.name}`,
-    tags: [apiInfo.parentDirName],
-    body: {
-        title: `查询${metaConfig.name}接口`,
-        type: 'object',
-        properties: {
-            page: metaConfig.schema.page,
-            limit: metaConfig.schema.limit
-        },
-        required: []
-    }
-};
+
 // 处理函数
-export default async function (fastify, opts) {
-    fastify.post(`/${apiInfo.pureFileName}`, {
-        schema: apiSchema,
-        handler: async function (req, res) {
+export default async (fastify) => {
+    // 当前文件的路径，fastify 实例
+    fnRoute(import.meta.url, fastify, {
+        // 接口名称
+        apiName: '查询邮件日志列表',
+        // 请求参数约束
+        schemaRequest: {
+            type: 'object',
+            properties: {
+                page: metaConfig.schema.page,
+                limit: metaConfig.schema.limit
+            },
+            required: []
+        },
+        // 返回数据约束
+        schemaResponse: {},
+        // 执行函数
+        apiHandler: async (req, res) => {
             try {
-                let mailLogModel = fastify.mysql //
+                const mailLogModel = fastify.mysql //
                     .table('sys_mail_log')
-                    .modify(function (queryBuilder) {
+                    .modify(function (qb) {
                         if (req.body.keyword !== undefined) {
-                            queryBuilder.where('nickname', 'like', `%${req.body.keyword}%`);
+                            qb.where('nickname', 'like', `%${req.body.keyword}%`);
                         }
                     });
 
                 // 记录总数
-                let { total } = await mailLogModel.clone().count('id', { as: 'total' }).first();
+                const { totalCount } = await mailLogModel.clone().selectCount();
 
                 // 记录列表
-                let rows = await mailLogModel
+                const rows = await mailLogModel
                     //
                     .clone()
                     .orderBy('created_at', 'desc')
-                    .offset(fnPageOffset(req.body.page, req.body.limit))
-                    .limit(req.body.limit)
-                    .select(selectKeys);
+                    .selectData(req.body.page, req.body.limit, fnSelectFields('./tables/mailLog.json'));
 
                 return {
                     ...codeConfig.SELECT_SUCCESS,
                     data: {
-                        total: total,
+                        total: totalCount,
                         rows: rows,
                         page: req.body.page,
                         limit: req.body.limit
@@ -63,4 +57,4 @@ export default async function (fastify, opts) {
             }
         }
     });
-}
+};
