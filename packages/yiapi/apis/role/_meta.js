1c2513f3b4e67589b9e44a8a89c7bd393d6dbcfb
chensuiyi <chenbimo@gmail.com>
2023-11-27 19:34:44 +0800

    调整配置(yiapi): 配置role目录meta文件

--- packages/yiapi/apis/role/_meta.js
+++ packages/yiapi/apis/role/_meta.js
@@ -1,19 +1,15 @@
-import { fnSchema } from '../../utils/index.js';
+import { fnSchema, fnMeta } from '../../utils/index.js';
 import { schemaField } from '../../config/schemaField.js';
 
-export let metaConfig = {
+export let metaConfig = fnMeta(import.meta.url, {
     name: '角色',
     schema: {
-        id: fnSchema(schemaField.id, '唯一ID'),
-        page: fnSchema(schemaField.page, '第几页'),
-        limit: fnSchema(schemaField.limit, '每页多少条'),
-        state: fnSchema(schemaField.state, '是否启用'),
         code: fnSchema(schemaField.code, '角色代号'),
         name: fnSchema(null, '角色名称', 'string', 1, 20),
         describe: fnSchema(schemaField.describe, '角色描述'),
         keyword: fnSchema(schemaField.keyword, '关键字'),
         menu_ids: {
-            title: '菜单ID组',
+            title: '菜单 ID 组',
             type: 'array',
             minItems: 0,
             maxItems: 10000,
@@ -22,7 +18,7 @@ export let metaConfig = {
             }
         },
         api_ids: {
-            title: '接口ID组',
+            title: '接口 ID 组',
             type: 'array',
             minItems: 0,
             maxItems: 10000,
@@ -31,4 +27,4 @@ export let metaConfig = {
             }
         }
     }
-};
+});
