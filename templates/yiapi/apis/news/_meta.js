729d171dffab33f64fd4f4cca4ae4abc5d8e9256
chensuiyi <chenbimo@gmail.com>
2023-11-28 18:45:54 +0800

    调整风格(yiapi-template): 修复fnSchema引用错误问题

--- templates/yiapi/apis/news/_meta.js
+++ templates/yiapi/apis/news/_meta.js
@@ -1,16 +1,13 @@
 import * as yiapi from '@yicode/yiapi';
 
-export let metaConfig = {
+export const metaConfig = yiapi.fnMeta(import.meta.url, {
     name: '资讯新闻',
     schema: {
-        id: yiapi.utils.fnSchema(yiapi.schemaField.id, '资讯 ID'),
-        page: yiapi.utils.fnSchema(yiapi.schemaField.page, '第几页'),
-        limit: yiapi.utils.fnSchema(yiapi.schemaField.limit, '每页数量'),
-        keyword: yiapi.utils.fnSchema(yiapi.schemaField.keyword, '搜索关键字'),
-        category_id: yiapi.utils.fnSchema(yiapi.schemaField.id, '资讯分类'),
-        title: yiapi.utils.fnSchema(yiapi.schemaField.title, '资讯标题'),
-        describe: yiapi.utils.fnSchema(yiapi.schemaField.string0to500, '资讯描述'),
-        thumbnail: yiapi.utils.fnSchema(yiapi.schemaField.image, '资讯缩略图'),
-        content: yiapi.utils.fnSchema(yiapi.schemaField.content, '资讯正文')
+        keyword: yiapi.fnSchema(yiapi.schemaField.keyword, '搜索关键字'),
+        category_id: yiapi.fnSchema(yiapi.schemaField.id, '资讯分类'),
+        title: yiapi.fnSchema(yiapi.schemaField.title, '资讯标题'),
+        describe: yiapi.fnSchema(yiapi.schemaField.string0to500, '资讯描述'),
+        thumbnail: yiapi.fnSchema(yiapi.schemaField.image, '资讯缩略图'),
+        content: yiapi.fnSchema(yiapi.schemaField.content, '资讯正文')
     }
-};
+});
