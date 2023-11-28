6c7fc2738442d8a19e7b756eb5872e50ac0e125a
chensuiyi <chenbimo@gmail.com>
2023-11-28 18:12:57 +0800

    新增功能(yiapi): 增加邮箱密码登录接口

---
+++ packages/yiapi/apis/user/emailPassLogin.js
@@ -0,0 +1,70 @@
+// 工具函数
+import { fnRoute, fnField } from '../../utils/index.js';
+// 配置文件
+import { codeConfig } from '../../config/codeConfig.js';
+import { metaConfig } from './_meta.js';
+
+export default async (fastify) => {
+    // 当前文件的路径，fastify 实例
+    fnRoute(import.meta.url, fastify, {
+        // 接口名称
+        apiName: '邮箱密码登录',
+        // 请求参数约束
+        schemaRequest: {
+            type: 'object',
+            properties: {
+                email: metaConfig.schema.email,
+                password: metaConfig.schema.password
+            },
+            required: ['email', 'password']
+        },
+        // 返回数据约束
+        schemaResponse: {},
+        // 执行函数
+        apiHandler: async (req, res) => {
+            try {
+                const userModel = fastify.mysql.table('sys_user');
+                const userData = await userModel //
+                    .clone()
+                    .where({ email: req.body.email })
+                    .selectOne(fnField('user', 'core', ['password']));
+
+                if (!userData?.id) {
+                    return {
+                        ...codeConfig.FAIL,
+                        msg: '用户不存在'
+                    };
+                }
+
+                // 如果没有设置密码，则提示设置密码后登录
+                if (!userData?.password) {
+                    return {
+                        ...codeConfig.FAIL,
+                        msg: '请扫码登录后，在个人中心绑定邮箱并设置密码后再使用此方式登录'
+                    };
+                }
+
+                if (userData.password !== fnSaltMD5(req.body.password)) {
+                    return {
+                        ...codeConfig.FAIL,
+                        msg: '密码错误'
+                    };
+                }
+
+                return {
+                    ...codeConfig.SUCCESS,
+                    msg: '密码登录成功',
+                    data: userData,
+                    token: await fastify.jwt.sign({
+                        id: userData.id,
+                        nickname: userData.nickname,
+                        role_codes: userData.role_codes
+                    })
+                };
+            } catch (err) {
+                fastify.log.error(err);
+                return codeConfig.INSERT_FAIL;
+            }
+        }
+    });
+};
