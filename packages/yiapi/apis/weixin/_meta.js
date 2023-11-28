567b5a47471f554a366c632eb252b5b0029ed848
chensuiyi <chenbimo@gmail.com>
2023-11-28 18:44:26 +0800

    调整配置(yiapi): 增加scan_qrcode_uuid验证字段

--- packages/yiapi/apis/weixin/_meta.js
+++ packages/yiapi/apis/weixin/_meta.js
@@ -6,8 +6,9 @@ export let metaConfig = fnMeta(import.meta.url, {
     schema: {
         order_no: fnSchema(schemaField.min1, '订单号'),
         buy_amount: fnSchema(schemaField.min1, '购买数量'),
-        pay_product: fnSchema(null, '支付产品', 'string', null, null, ['redYhq']),
-        from_product: fnSchema(null, '来自产品', 'string', null, null, ['no', 'redYhq']),
-        agent_id: fnSchema(schemaField.pid, '代理 ID')
+        pay_product: fnSchema(null, '支付产品', 'string', null, null, ['no']),
+        from_product: fnSchema(null, '来自产品', 'string', null, null, ['no']),
+        agent_id: fnSchema(schemaField.pid, '代理 ID'),
+        scan_qrcode_uuid: fnSchema(null, '扫码识别号', 'string', 5, 50)
     }
 });
