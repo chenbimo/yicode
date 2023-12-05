62ba4bf59b809c436b0eb8bc1b1d3bede027a73c
chensuiyi <chenbimo@gmail.com>
2023-12-05 10:32:20 +0800

    优化性能(yiapi): 将fnObjTraverse函数去除，避免循环调用引起的内存溢出

--- packages/yiapi/utils/index.js
+++ packages/yiapi/utils/index.js
@@ -337,60 +337,60 @@ export function fnApiParamsSign(params) {
 }
 
 // 深度遍历对象节点
-export let fnObjTraverse = (obj, callbacks = null, flattenArray = false, level = 0, path = []) => {
-    let processValue = null;
-    if (callbacks && callbacks.processValue) {
-        processValue = callbacks.processValue;
-    }
-    if (callbacks && callbacks.enterLevel) {
-        callbacks.enterLevel(level, path);
-    }
-    Object.entries(obj).forEach(([key, val]) => {
-        if (val !== null && typeof val == 'object' && (!Array.isArray(val) || !flattenArray)) {
-            if (Array.isArray(val)) {
-                for (let i = 0; i < val.length; i++) {
-                    let elem = val[i];
-                    let itemKey = '_' + i;
-                    let currentPath = Array.from(path);
-                    currentPath.push(key);
-                    if (elem !== null && typeof elem == 'object') {
-                        if (processValue) {
-                            processValue(itemKey, elem, level, currentPath, true, true, (newElem) => {
-                                obj[key][i] = newElem;
-                            });
-                        }
-                        currentPath.push(itemKey);
-                        fnObjTraverse(elem, callbacks, flattenArray, level + 1, currentPath);
-                    } else {
-                        if (processValue) {
-                            processValue(itemKey, elem, level, currentPath, false, true, (newElem) => {
-                                obj[key][i] = newElem;
-                            });
-                        }
-                    }
-                }
-            } else {
-                if (processValue) {
-                    processValue(key, val, level, path, true, false, (newVal) => {
-                        obj[key] = newVal;
-                    });
-                }
-                let currentPath = Array.from(path);
-                currentPath.push(key);
-                fnObjTraverse(val, callbacks, flattenArray, level + 1, currentPath);
-            }
-        } else {
-            if (processValue) {
-                processValue(key, val, level, path, false, false, (newVal) => {
-                    obj[key] = newVal;
-                });
-            }
-        }
-    });
-    if (callbacks && callbacks.exitLevel) {
-        callbacks.exitLevel(level, path);
-    }
-};
+// export let fnObjTraverse = (obj, callbacks = null, flattenArray = false, level = 0, path = []) => {
+//     let processValue = null;
+//     if (callbacks && callbacks.processValue) {
+//         processValue = callbacks.processValue;
+//     }
+//     if (callbacks && callbacks.enterLevel) {
+//         callbacks.enterLevel(level, path);
+//     }
+//     Object.entries(obj).forEach(([key, val]) => {
+//         if (val !== null && typeof val == 'object' && (!Array.isArray(val) || !flattenArray)) {
+//             if (Array.isArray(val)) {
+//                 for (let i = 0; i < val.length; i++) {
+//                     let elem = val[i];
+//                     let itemKey = '_' + i;
+//                     let currentPath = Array.from(path);
+//                     currentPath.push(key);
+//                     if (elem !== null && typeof elem == 'object') {
+//                         if (processValue) {
+//                             processValue(itemKey, elem, level, currentPath, true, true, (newElem) => {
+//                                 obj[key][i] = newElem;
+//                             });
+//                         }
+//                         currentPath.push(itemKey);
+//                         fnObjTraverse(elem, callbacks, flattenArray, level + 1, currentPath);
+//                     } else {
+//                         if (processValue) {
+//                             processValue(itemKey, elem, level, currentPath, false, true, (newElem) => {
+//                                 obj[key][i] = newElem;
+//                             });
+//                         }
+//                     }
+//                 }
+//             } else {
+//                 if (processValue) {
+//                     processValue(key, val, level, path, true, false, (newVal) => {
+//                         obj[key] = newVal;
+//                     });
+//                 }
+//                 let currentPath = Array.from(path);
+//                 currentPath.push(key);
+//                 fnObjTraverse(val, callbacks, flattenArray, level + 1, currentPath);
+//             }
+//         } else {
+//             if (processValue) {
+//                 processValue(key, val, level, path, false, false, (newVal) => {
+//                     obj[key] = newVal;
+//                 });
+//             }
+//         }
+//     });
+//     if (callbacks && callbacks.exitLevel) {
+//         callbacks.exitLevel(level, path);
+//     }
+// };
 
 /**
  * 检查传参有效性
