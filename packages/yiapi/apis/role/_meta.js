fe48b26b527170704f6c70cb9e861dad4c7be562
chensuiyi <chenbimo@gmail.com>
2023-12-11 21:00:58 +0800

    修复问题(yiapi): 修复role/_meta.js文件中keyword协议错误的问题

--- packages/yiapi/apis/role/_meta.js
+++ packages/yiapi/apis/role/_meta.js
@@ -3,13 +3,11 @@ import { schemaField } from '../../config/schemaField.js';
 import { schemaRegexp } from '../../config/schemaRegexp.js';
 
 export let metaConfig = fnMeta(import.meta.url, {
-    name: '角色',
-    schema: {
-        code: fnSchema(null, '角色代号', 'string', 1, 20, '', null, schemaRegexp.aA0_),
-        name: fnSchema(null, '角色名称', 'string', 0, 30),
-        describe: fnSchema(null, '角色描述', 'string', 0, 100),
-        keyword: fnSchema(schemaField.keyword, '关键字'),
-        menu_ids: fnSchema(null, '菜单ID组', 'array', 0, 10000, [], null, 'integer', true),
-        api_ids: fnSchema(null, '接口ID组', 'array', 0, 10000, [], null, 'integer', true)
-    }
+    _name: '角色',
+    code: fnSchema(null, '角色代号', 'string', 1, 20, '', null, schemaRegexp.aA0_),
+    name: fnSchema(null, '角色名称', 'string', 0, 30),
+    describe: fnSchema(null, '角色描述', 'string', 0, 100),
+    keyword: fnSchema(null, '关键字', 'string', 0, 100),
+    menu_ids: fnSchema(null, '菜单ID组', 'array', 0, 10000, [], null, 'integer', true),
+    api_ids: fnSchema(null, '接口ID组', 'array', 0, 10000, [], null, 'integer', true)
 });
