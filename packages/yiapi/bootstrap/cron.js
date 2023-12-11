2f0096cb5eaa65c67734d76f971e765745baf1c5
chensuiyi <chenbimo@gmail.com>
2023-12-11 09:10:04 +0800

    重构代码(yiapi): 调整bootstrap/cron.js代码

--- packages/yiapi/bootstrap/cron.js
+++ packages/yiapi/bootstrap/cron.js
@@ -8,14 +8,6 @@ import * as utils from '../utils/index.js';
 
 // 配置信息
 import { appConfig } from '../config/appConfig.js';
-import { codeConfig } from '../config/codeConfig.js';
-import { crosConfig } from '../config/crosConfig.js';
-import { fieldType } from '../config/fieldType.js';
-import { logConfig } from '../config/logConfig.js';
-import { schemaField } from '../config/schemaField.js';
-import { schemaType } from '../config/schemaType.js';
-import { sysConfig } from '../config/sysConfig.js';
-import { tableField } from '../config/tableField.js';
 
 async function plugin(fastify, opts) {
     appConfig.cron.forEach((item) => {
@@ -23,21 +15,13 @@ async function plugin(fastify, opts) {
             console.log(logSymbols.error, `${item.name} 定时器 handler 必须为一个函数`);
             process.exit();
         }
-        let options = { name: item.name, maxRuns: item.maxRuns, timezone: item.timezone };
-        let yiapi = {
-            fastify,
-            utils,
-            appConfig,
-            codeConfig,
-            crosConfig,
-            fieldType,
-            schemaField,
-            schemaType,
-            sysConfig,
-            tableField
+        const options = {
+            name: item.name,
+            maxRuns: item.maxRuns,
+            timezone: item.timezone
         };
-        let job = Cron(item.timer, options, () => {
-            item.handler(yiapi);
+        const job = Cron(item.timer, options, () => {
+            item.handler(appConfig);
         });
         fastify.decorate(item.code, job);
     });
