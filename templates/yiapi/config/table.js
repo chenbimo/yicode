b5965ebb17a5deacbeb0d2cd5eb5bec43c3859cb
chensuiyi <chenbimo@gmail.com>
2023-12-12 11:15:59 +0800

    新增功能(yiapi-template): openid字段内置到系统user表中

--- templates/yiapi/config/table.js
+++ templates/yiapi/config/table.js
@@ -6,12 +6,6 @@ const tableConfig = {
             comment: '测试字段，此字段不能删除，用来验证自定义字段是否生效，避免同步时误删字段',
             length: 10,
             default: ''
-        },
-        openid: {
-            type: 'string',
-            comment: '微信 openid',
-            length: 50,
-            default: ''
         }
     }
 };
