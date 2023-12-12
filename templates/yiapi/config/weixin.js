bc61d4513ec76b1d58ecd5fbe66b43467bdd180e
chensuiyi <chenbimo@gmail.com>
2023-12-12 11:16:19 +0800

    新增功能(yiapi-template): 微信商户和微信公众号配置格式调整

--- templates/yiapi/config/weixin.js
+++ templates/yiapi/config/weixin.js
@@ -1,23 +1,33 @@
 import { weixinPrivateKey } from './key.js';
 export const weixinConfig = {
     // 商户
-    merchant: [
-        {
-            code: 'merchant-1',
+    merchant: {
+        mchId_123456: {
             name: '商户1',
             mchId: '123456',
             serialNo: '123456789123456789123456',
             apiv3PrivateKey: '123456789123456789123456',
             privateKey: weixinPrivateKey
+        },
+        mchId_123457: {
+            name: '商户2',
+            mchId: '123456',
+            serialNo: '123456789123456789123456',
+            apiv3PrivateKey: '123456789123456789123456',
+            privateKey: weixinPrivateKey
         }
-    ],
+    },
     // 公众号
-    account: [
-        {
-            code: 'gongzhong-1',
+    gongZhong: {
+        appId_wx123456123456: {
             name: '公众号1',
             appId: 'wx123456123456',
             appSecret: '123456789123456789123456'
+        },
+        appId_wx123456123457: {
+            name: '公众号2',
+            appId: 'wx123456123456',
+            appSecret: '123456789123456789123456'
         }
-    ]
+    }
 };
