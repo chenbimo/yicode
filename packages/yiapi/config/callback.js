a25671e34f7cdd37da1b15035743f8617e61b416
chensuiyi <chenbimo@gmail.com>
2023-12-12 14:20:30 +0800

    思考计划(yiapi): 删除内核的callback.js文件

--- packages/yiapi/config/callback.js
+++
@@ -1,6 +0,0 @@
-export const callbackConfig = {
-    // 微信消息回调
-    weixinMessage(fastify, options) {
-        console.log('🚀 ~ file: callback.js:2 ~ messageCallback ~ options:', options);
-    }
-};
