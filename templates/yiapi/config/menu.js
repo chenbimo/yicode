206c51abc399fdcd857a4797bd269e59ed21bd13
chensuiyi <chenbimo@gmail.com>
2023-12-13 09:30:33 +0800

    调整配置(yiapi): 增加自定义菜单演示注释

--- templates/yiapi/config/menu.js
+++ templates/yiapi/config/menu.js
@@ -1,4 +1,16 @@
-// 系统表字段扩展
-const menuConfig = {};
+// 自定义菜单
+const menuConfig = {
+    // 自定义菜单演示
+    // '/keyword': {
+    //     name: '关键字',
+    //     sort: 2,
+    //     children: {
+    //         '/keyword/list': {
+    //             name: '关键字列表',
+    //             sort: 1
+    //         }
+    //     }
+    // }
+};
 
 export { menuConfig };
