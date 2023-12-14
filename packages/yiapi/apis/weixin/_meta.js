e4d9997fc5ab876641fc68295c44221d07eeeb0f
chensuiyi <chenbimo@gmail.com>
2023-12-14 17:27:49 +0800

    调整配置(yiapi): 调整购买时长参数的协议

--- packages/yiapi/apis/weixin/_meta.js
+++ packages/yiapi/apis/weixin/_meta.js
@@ -18,7 +18,7 @@ export const metaConfig = fnMeta(import.meta.url, {
     from_product: fnSchema(null, '来自产品', 'string', null, null, productNames),
     agent_id: fnSchema(schemaField.min0, '代理ID'),
     // 购买相关
-    buy_duration: fnSchema(null, '购买时长', 'string', null, null, null, durationTime),
+    buy_duration: fnSchema(null, '时长代号', 'string', 1, 100),
     buy_amount: fnSchema(schemaField.min1, '购买数量'),
     buy_product: fnSchema(null, '支付产品', 'string', null, null, productNames),
     buy_note: fnSchema(null, '购买备注', 'string', 0, 100, '')
