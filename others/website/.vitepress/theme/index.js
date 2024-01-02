59a372a43712b38d17cdf8e58d2aed9f4d99cb3c
chensuiyi <chenbimo@gmail.com>
2024-01-03 00:59:37 +0800

    思考计划: 去掉others空间

--- others/website/.vitepress/theme/index.js
+++
@@ -1,9 +0,0 @@
-import DefaultTheme from 'vitepress/theme';
-import './style/custom.css';
-import homeMore from './components/homeMore.vue';
-export default {
-    extends: DefaultTheme,
-    enhanceApp(ctx) {
-        ctx.app.component('homeMore', homeMore);
-    }
-};
