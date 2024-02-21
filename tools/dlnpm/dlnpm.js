429d6cc3ebf62d4cc3ab4e00e9a76a02ae98468b
chensuiyi <chenbimo@gmail.com>
2024-02-21 14:42:36 +0800

    调整配置(dlnpm): 修改模板下载地址配置等

--- tools/dlnpm/dlnpm.js
+++ tools/dlnpm/dlnpm.js
@@ -6,20 +6,20 @@ import pacote from 'pacote';
 import logSymbols from 'log-symbols';
 
 // 仓库地址配置
-let registryConfig = {
+const registryConfig = {
     'npmmirror.com': 'https://registry.npmmirror.com',
     'npmjs.com': 'https://registry.npmjs.com'
 };
 
 async function main() {
     try {
-        let promptData = {};
+        const promptData = {};
 
         console.log(logSymbols.info, '开发者：随易科技（https://yicode.tech）');
         console.log('-----------------------------------------');
 
         // 下载类型
-        let { type } = await inquirer.prompt({
+        const { type } = await inquirer.prompt({
             type: 'list',
             name: 'type',
             message: '选择下载类型',
@@ -38,7 +38,7 @@ async function main() {
         promptData.type = type;
 
         // 从哪里下载
-        let { registry } = await inquirer.prompt({
+        const { registry } = await inquirer.prompt({
             type: 'list',
             name: 'registry',
             message: '选择从哪里下载',
@@ -59,27 +59,31 @@ async function main() {
 
         if (promptData.type === 'yicode') {
             // 下载什么内容
-            let { template } = await inquirer.prompt({
+            const { template } = await inquirer.prompt({
                 type: 'list',
                 name: 'template',
                 message: '选择要下载的包',
-                default: '@yicode-template/base-vue3',
+                default: '@yicode/yibase',
                 choices: [
                     {
+                        name: 'yiapi Node.js接口开发模板',
+                        value: '@yicode/yiapi'
+                    },
+                    {
                         name: 'yite + vue3 基础项目开发模板',
-                        value: '@yicode-template/base-vue3'
+                        value: '@yicode/yibase'
                     },
                     {
                         name: 'yite + vue3 后台项目开发模板',
-                        value: '@yicode-template/admin-vue3'
+                        value: '@yicode/yiadmin'
                     },
                     {
                         name: 'uniapp + vue3 基础项目开发模板',
-                        value: '@yicode-template/uni-vue3'
+                        value: '@yicode/yiuni'
                     },
                     {
                         name: 'yidocs 易文档基础模板',
-                        value: '@yicode-template/yidocs'
+                        value: '@yicode/yidocs'
                     }
                 ]
             });
@@ -89,7 +93,7 @@ async function main() {
 
         if (promptData.type === 'other') {
             // 下载什么内容
-            let { template } = await inquirer.prompt({
+            const { template } = await inquirer.prompt({
                 type: 'input',
                 name: 'template',
                 message: '请输入要下载的包名称',
@@ -108,7 +112,7 @@ async function main() {
         }
 
         // 下载什么版本
-        let { version } = await inquirer.prompt({
+        const { version } = await inquirer.prompt({
             type: 'input',
             name: 'version',
             message: '输入要下载的版本（默认下载最新版本）',
@@ -119,7 +123,7 @@ async function main() {
 
         try {
             const metaData = await got.get(`${registryConfig[promptData.registry]}/${promptData.template}/${promptData.version}`).json();
-            let downMeta = await pacote.extract(metaData.dist.tarball, './npm-package', {});
+            const downMeta = await pacote.extract(metaData.dist.tarball, './npm-package', {});
             console.log(logSymbols.success, '资源已下载到默认的 npm-package 目录，请移动到正确的目录!');
         } catch (error) {
             console.log(logSymbols.error, '资源错误或不存在，请检查包名或版本是否正确!');
