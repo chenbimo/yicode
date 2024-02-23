c1e7d4c18e1837846c91f6eafe6b3734f5711095
chensuiyi <chenbimo@gmail.com>
2024-02-23 23:06:17 +0800

    完善功能(yiapi): 时间同步增加try catch，避免因为获取时间失败引起的无法运行接口

--- packages/yiapi/bootstrap/syncTime.js
+++ packages/yiapi/bootstrap/syncTime.js
@@ -3,19 +3,23 @@ import got from 'got';
 import { differenceInSeconds, toDate } from 'date-fns';
 
 async function plugin(fastify, opts) {
-    const res = await got('http://api.m.taobao.com/rest/api3.do?api=mtop.common.getTimestamp', {
-        method: 'get'
-    }).json();
-    const timestamp = res?.data?.t;
-    if (timestamp) {
-        const diffSeconds = Math.abs(differenceInSeconds(new Date(), toDate(timestamp * 1)));
-        if (diffSeconds > 180) {
-            fastify.log.error('与北京时间相差大于1分钟');
+    try {
+        const res = await got('http://api.m.taobao.com/rest/api3.do?api=mtop.common.getTimestamp', {
+            method: 'get'
+        }).json();
+        const timestamp = res?.data?.t;
+        if (timestamp) {
+            const diffSeconds = Math.abs(differenceInSeconds(new Date(), toDate(timestamp * 1)));
+            if (diffSeconds > 180) {
+                fastify.log.error('与北京时间相差大于1分钟');
+                process.exit();
+            }
+        } else {
+            fastify.log.error('北京时间未获取，无法校对服务器时间');
             process.exit();
         }
-    } else {
-        fastify.log.error('北京时间未获取，无法校对服务器时间');
-        process.exit();
+    } catch (err) {
+        fastify.log.error(err);
     }
 }
 export default fp(plugin, { name: 'syncTime' });
