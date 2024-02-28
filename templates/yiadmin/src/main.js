b43787063f6826f040ec8c98326816953b7d67fe
chensuiyi <chenbimo@gmail.com>
2024-02-28 11:48:17 +0800

    调整配置(yiadmin,yibase): 默认导入arco-design图标

--- templates/yiadmin/src/main.js
+++ templates/yiadmin/src/main.js
@@ -1,5 +1,6 @@
 import App from '@/App.vue';
 import '@arco-design/web-vue/dist/arco.css';
+import ArcoVueIcon from '@arco-design/web-vue/es/icon';
 import 'virtual:uno.css';
 
 const app = createApp(App);
@@ -9,5 +10,6 @@ const $Pinia = Pinia.createPinia();
 app.use($Router);
 app.use($Pinia);
 app.use($I18n);
+app.use(ArcoVueIcon);
 
 app.mount('#app');
