648f02c5f3e458833e0fa05d90567045ad40df11
chensuiyi <chenbimo@gmail.com>
2024-02-29 18:32:41 +0800

    完善功能(yidash): datetime_relativeTime方法同时返回原来的值

--- packages/yidash/lib/datetime_relativeTime.js
+++ packages/yidash/lib/datetime_relativeTime.js
@@ -3,15 +3,16 @@ import { zhCN } from 'date-fns/locale';
 // 转换相对时间
 const _convertTime = (obj) => {
     try {
-        let item = {};
+        const item = {};
         for (let key in obj) {
             if (obj.hasOwnProperty(key)) {
-                let value = obj[key];
+                const value = obj[key];
                 if (key.endsWith('_at')) {
                     let key1 = key.replace('_at', '_at1');
                     let key2 = key.replace('_at', '_at2');
                     let dt = new Date(value);
                     if (value !== 0) {
+                        item[key] = value;
                         item[key1] = format(dt, 'yyyy-MM-dd HH:mm:ss');
                         item[key2] = formatDistanceToNow(dt, { locale: zhCN, addSuffix: true });
                     } else {
