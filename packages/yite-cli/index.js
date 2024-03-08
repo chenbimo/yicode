e6a53eecf1b5995829333c7309528c31f7853276
chensuiyi <chenbimo@gmail.com>
2024-03-08 10:32:05 +0800

    新增功能(yite-cli): 导出unoCss配置

--- packages/yite-cli/index.js
+++ packages/yite-cli/index.js
@@ -1,4 +1,8 @@
 import { isCookieEnabled, getCookie, getRawCookie, getAllCookies, setCookie, removeCookie } from 'tiny-cookie';
+import { defineConfig } from 'unocss';
+import { unocssConfig } from './unocss.js';
+
+const defineUnocssConfig = defineConfig(unocssConfig);
 
 export {
     //
@@ -7,5 +11,6 @@ export {
     getRawCookie,
     getAllCookies,
     setCookie,
-    removeCookie
+    removeCookie,
+    defineUnocssConfig
 };
