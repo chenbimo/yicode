b13e12f4b7fed3f1c6b0ad076fee92522249d2be
chensuiyi <chenbimo@gmail.com>
2024-03-16 17:41:48 +0800

    调整配置(yibase): 调整unocss配置写法

--- templates/yibase/unocss.js
+++ templates/yibase/unocss.js
@@ -1,29 +1,3 @@
-import path from 'node:path';
-import { fnFileProtocolPath, fnOmit, fnImport, fnRequire } from './utils.js';
-import { cliDir, appDir, srcDir, cacheDir } from './config.js';
-
-// unocss相关配置
-import { presetAttributify, presetUno, presetIcons } from 'unocss';
-import transformerVariantGroup from '@unocss/transformer-variant-group';
-import transformerCompileClass from '@unocss/transformer-compile-class';
-import transformerDirectives from '@unocss/transformer-directives';
-
-// const { yiteConfig } = await fnImport(fnFileProtocolPath(path.resolve(srcDir, 'yite.config.js')), 'yiteConfig', {});
-const { yiteConfig } = fnRequire('yite.config.js', {});
-export const unocssConfig = Object.assign(
-    {
-        presets: [
-            //
-            presetUno(),
-            presetAttributify()
-        ],
-        transformers: [
-            //
-            transformerDirectives(),
-            transformerVariantGroup(),
-            transformerCompileClass()
-        ],
-        rules: []
-    },
-    yiteConfig?.unocssConfig || {}
-);
+import { unocssConfig } from '@yicode/yite-cli/unocss.js';
+import { defineConfig } from 'unocss';
+export default defineConfig(unocssConfig);
