9c1640ab63524b5f971f5f102bf35fce62e7c300
chensuiyi <chenbimo@gmail.com>
2024-05-19 11:21:03 +0800

    暂存代码: 保存代码

--- packages/yiapi/bootstrap/syncApi.js
+++ packages/yiapi/bootstrap/syncApi.js
@@ -42,10 +42,10 @@ async function syncApiDir(fastify) {
         const apiModel = fastify.mysql.table('sys_api');
 
         // 所有的接口元数据文件，用来生成目录
-        const allApiMeta = await fnAllApiFiles('meta');
+        const allApiMetaFiles = await fnAllApiFiles('meta');
 
         // 所有目录路径的数组
-        const allApiMetaByValue = allApiMeta.map((file) => {
+        const allApiMetaByValue = allApiMetaFiles.map((file) => {
             return file.replace('/_meta.js', '').replace(/.+\/apis/, '');
         });
 
@@ -53,50 +53,38 @@ async function syncApiDir(fastify) {
         const apis = await apiModel.clone().selectAll();
 
         // 所有接口目录数据
-        const apisDir = apis.filter((item) => item.is_bool === 0);
-        const apiDirValue = apisDir.map((item) => item.value);
-        const apiDirByValue = toKeyBy(apisDir, 'value');
+        const apiDirDb = apis.filter((item) => item.is_bool === 0);
+        const apiDirDbByValue = toKeyBy(apiDirDb, 'value');
 
-        // 从数据库查出的，明确保留的接口
-        const keepApiDataObject = [];
-        // 将要删除的接口数据
         const deleteApiDirData = [];
-        // 将要添加的接口数据
         const insertApiDirData = [];
-        // 将要修改的数据
         const updateApiDirData = [];
 
         // 找出所有需要删除的接口目录
-        apisDir.forEach((item) => {
+        apiDirDb.forEach((item) => {
             if (allApiMetaByValue.includes(item.value) === false) {
                 deleteApiDirData.push(item.id);
-            } else {
-                if (keepApiDataObject.includes(item.value)) {
-                    deleteApiDirData.push(item.id);
-                } else {
-                    keepApiDataObject.push(item.value);
-                }
             }
         });
 
-        for (let i = 0; i < allApiMeta.length; i++) {
-            const file = allApiMeta[i];
+        for (let i = 0; i < allApiMetaFiles.length; i++) {
+            const file = allApiMetaFiles[i];
             const apiDirName = file.replace('/_meta.js', '').replace(/.+\/apis/, '');
 
             // 如果数据库中存在当前接口目录，则进行添加或更新
-            const { metaConfig } = await fnImportAbsolutePath(url.pathToFileURL(file), {});
+            const { metaConfig } = await fnImportAbsolutePath(file, 'metaConfig', {});
 
             const apiMeta = {
-                name: metaConfig._name,
+                name: metaConfig.dirName,
                 value: apiDirName,
                 is_bool: 0,
                 pid: 0,
                 pids: '0'
             };
 
-            if (apiDirByValue[apiDirName]) {
+            if (apiDirDbByValue[apiDirName]) {
                 // 如果数据库中已有此目录，则更新目录
-                apiMeta.id = apiDirByValue[apiDirName].id;
+                apiMeta.id = apiDirDbByValue[apiDirName].id;
                 updateApiDirData.push(apiMeta);
             } else {
                 // 如果数据库中没有此目录，则添加目录
@@ -148,20 +136,17 @@ async function syncApiFile(fastify) {
         });
 
         // 接口目录同步完毕后，重新查询一遍接口目录，拿到所有的接口目录
-        const apis = await apiModel.clone().selectAll();
+        const apiDb = await apiModel.clone().selectAll();
 
         // 所有接口目录数据
-        const apisDir = apis.filter((item) => item.is_bool === 0);
-        const apiDirValue = apisDir.map((item) => item.value);
-        const apiDirByValue = toKeyBy(apisDir, 'value');
+        const apiDirDb = apiDb.filter((item) => item.is_bool === 0);
+        const apiDirDbByValue = toKeyBy(apiDirDb, 'value');
 
         // 所有的接口数据
-        const apisFile = apis.filter((item) => item.is_bool === 1);
-        const apiFileValue = apisFile.map((item) => item.value);
-        const apiFileByValue = toKeyBy(apisFile, 'value');
+        const apiFileDb = apiDb.filter((item) => item.is_bool === 1);
+        const apiFileValue = apiFileDb.map((item) => item.value);
+        const apiFileByValue = toKeyBy(apiFileDb, 'value');
 
-        // 从数据库查出的，明确保留的接口
-        const keepApiDataObject = [];
         // 将要删除的接口数据
         const deleteApiData = [];
         // 将要添加的接口数据
@@ -172,36 +157,30 @@ async function syncApiFile(fastify) {
         const autoApiObject = {};
 
         // 找出所有需要删除的接口文件
-        apisFile.forEach((item) => {
+        apiFileDb.forEach((item) => {
             if (allApiFileByValue.includes(item.value) === false) {
                 deleteApiData.push(item.id);
-            } else {
-                if (keepApiDataObject.includes(item.value)) {
-                    deleteApiData.push(item.id);
-                } else {
-                    keepApiDataObject.push(item.value);
-                }
             }
         });
 
         // 遍历项目接口文件
-        // 使用 for 替代 forEach 遍历，不然代码不会同步执行
         for (let i = 0; i < allApiFiles.length; i++) {
             const file = allApiFiles[i];
-            const apiFileName = file.replace('.js', '').replace(/.+\/apis/, '');
-            // 获得父级数据
-            const apiDirData = apiDirByValue[dirname(apiFileName)] || {};
+            const apiFileName = basename(file, '.js');
+            const apiDirName = dirname(file);
+            const apiFileRoute = file.replace('.js', '').replace(/.+\/apis/, '');
+            const apiDirRoute = dirname(apiFileRoute);
+            const apiDirData = apiDirDbByValue[apiDirRoute] || {};
 
-            const { apiName } = await fnImportAbsolutePath(file, {});
+            const { metaConfig } = await fnImportAbsolutePath(resolve(apiDirName, '_meta.js'), 'metaConfig', {});
 
-            if (apiFileValue.includes(apiFileName) === false && !autoApiObject[apiFileName]) {
+            if (apiFileValue.includes(apiFileRoute) === false) {
                 // 如果当前接口在数据库中不存在，且没有添加过，则添加接口
                 // 防止2个同名接口重复添加
-                autoApiObject[apiFileName] = true;
-                let apiParams = {
+                const apiParams = {
                     pid: 0,
-                    name: apiName || '',
-                    value: apiFileName,
+                    name: metaConfig.apiNames[apiFileName] || '',
+                    value: apiFileRoute,
                     sort: 0,
                     is_open: 0,
                     describe: '',
@@ -212,7 +191,7 @@ async function syncApiFile(fastify) {
                 if (appConfig.tablePrimaryKey === 'time') {
                     apiParams.id = fnIncrUID();
                 }
-                if (_isEmpty(apiDirData) === false) {
+                if (apiDirData?.id) {
                     apiParams.pid = apiDirData.id;
                     apiParams.pids = `0,${apiDirData.id}`;
                     apiParams.level = 2;
@@ -220,32 +199,29 @@ async function syncApiFile(fastify) {
                 insertApiData.push(apiParams);
             } else {
                 // 如果当前接口存在，且父级接口目录也存在，且父级pid为0的野生接口，则更新修改接口文件信息
-                if (!autoApiObject[apiFileName]) {
-                    autoApiObject[apiFileName] = true;
 
-                    // 当前API数据
-                    const currentApi = apiFileByValue[apiFileName] || {};
-                    if (_isEmpty(currentApi) === false) {
-                        // 如果当前API不为空，且父级API不为空，且当前父级ID为0
-                        if (_isEmpty(apiDirData) === false) {
-                            const params = {
-                                id: currentApi.id,
-                                pid: apiDirData.id,
-                                pids: `0,${apiDirData.id}`,
-                                level: 2,
-                                name: apiName || ''
-                            };
-                            updateApiData.push(params);
-                        } else {
-                            const params = {
-                                id: currentApi.id,
-                                pid: 0,
-                                pids: `0`,
-                                level: 1,
-                                name: apiName || ''
-                            };
-                            updateApiData.push(params);
-                        }
+                // 当前API数据
+                const currentApi = apiFileByValue[apiFileRoute] || {};
+                if (_isEmpty(currentApi) === false) {
+                    // 如果当前API不为空，且父级API不为空，且当前父级ID为0
+                    if (_isEmpty(apiDirData) === false) {
+                        const params = {
+                            id: currentApi.id,
+                            pid: apiDirData.id,
+                            pids: `0,${apiDirData.id}`,
+                            level: 2,
+                            name: apiName || ''
+                        };
+                        updateApiData.push(params);
+                    } else {
+                        const params = {
+                            id: currentApi.id,
+                            pid: 0,
+                            pids: `0`,
+                            level: 1,
+                            name: apiName || ''
+                        };
+                        updateApiData.push(params);
                     }
                 }
             }
