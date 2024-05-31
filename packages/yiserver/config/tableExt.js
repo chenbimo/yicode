4df2782ac79547406ded44ed5f350013db928054
chensuiyi <chenbimo@gmail.com>
2024-05-31 22:19:06 +0800

    重构代码(yiapi): 重新定义表扩展

--- packages/yiserver/config/tableExt.js
+++ packages/yiserver/config/tableExt.js
@@ -3,15 +3,10 @@ export const tableExtConfig = {
     sys_user: {
         test_field: {
             name: '测试字段，此字段不能删除',
-            field: {
-                type: 'string',
-                default: ''
-            },
-            schema: {
-                type: 'string',
-                min: 1,
-                max: 50
-            }
+            type: 'string',
+            default: '',
+            min: 1,
+            max: 50
         }
     }
 };
