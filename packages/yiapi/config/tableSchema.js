6b6a97e6f590f9ec8babb151c35a94161d2bb308
chensuiyi <chenbimo@gmail.com>
2024-05-31 11:18:14 +0800

    调整配置(yiapi): 删除接口验证的的array类型

--- packages/yiapi/config/tableSchema.js
+++ packages/yiapi/config/tableSchema.js
@@ -1,6 +1,5 @@
 export const tableSchemaConfig = {
     string: ['type', 'default', 'min', 'max', 'pattern', 'enum'],
     integer: ['type', 'default', 'min', 'max', 'multipleOf', 'enum'],
-    number: ['type', 'default', 'min', 'max', 'multipleOf', 'enum'],
-    array: ['type', 'default', 'min', 'max', 'items', 'isUnique', 'isAdditional']
+    number: ['type', 'default', 'min', 'max', 'multipleOf', 'enum']
 };
