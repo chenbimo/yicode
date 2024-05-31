5db41fddb1dc42beedc62c9cb13dd39cfba41cf3
chensuiyi <chenbimo@gmail.com>
2024-05-31 22:12:25 +0800

    重构代码(yiapi): 调整表字段验证格式

--- packages/yiapi/schema/table.js
+++ packages/yiapi/schema/table.js
@@ -12,53 +12,194 @@ const fieldTypeEnum = [
     'float',
     'double'
 ];
-const schemaTypeEnum = ['string', 'integer', 'number'];
+
+// {
+//     name: { title: '字段名称', type: 'string' },
+//     default: { title: '默认值', type: ['string', 'integer', 'number'] },
+//     isIndex: { title: '是否索引', type: 'boolean', default: false },
+//     isUnique: { title: '是否唯一', type: 'boolean', default: false },
+//     isUnsigned: { title: '是否无符号', type: 'boolean', default: false },
+//     precision: { title: '整数精度', type: 'integer', minimum: 0, default: 8 },
+//     scale: { title: '小数精度', type: 'integer', minimum: 0, default: 2 },
+//     default2: { title: '默认值', type: ['string', 'integer', 'number'] },
+//     min: { title: '最小长度', type: 'integer', minimum: 0 },
+//     max: { title: '最大长度', type: 'integer' },
+//     enum: { title: '枚举值', type: 'array' },
+//     pattern: { title: '模式匹配', type: 'string' },
+//     multipleOf: { title: '倍数', type: 'integer', minimum: 1 }
+// }
+
 export const tableSchema = {
     title: '数据库表',
     type: 'object',
+    additionalProperties: false,
     patternProperties: {
         '^[a-z][a-z0-9_]*$': {
             title: '任意字段',
             type: 'object',
             properties: {
-                name: {
-                    title: '字段名称',
-                    type: 'string'
-                },
-                type: {
-                    title: '数据类型',
-                    type: 'string',
-                    enum: fieldTypeEnum
-                },
-                field: {
-                    type: 'object',
+                type: { title: '数据类型', type: 'string', enum: fieldTypeEnum }
+            },
+            required: ['type'],
+            oneOf: [
+                {
                     properties: {
-                        default: { title: '默认值', type: ['string', 'integer', 'number'] },
+                        type: { const: 'string' },
+                        name: { title: '字段名称', type: 'string' },
+                        default: { title: '默认值', type: 'string' },
+                        isIndex: { title: '是否索引', type: 'boolean', default: false },
+                        isUnique: { title: '是否唯一', type: 'boolean', default: false },
+                        default2: { title: '默认值', type: 'string' },
+                        min: { title: '最小长度', type: 'integer', minimum: 0 },
+                        max: { title: '最大长度', type: 'integer' },
+                        enum: { title: '枚举值', type: 'array' },
+                        pattern: { title: '模式匹配', type: 'string' }
+                    },
+                    additionalProperties: false
+                },
+                {
+                    properties: {
+                        type: { const: 'mediumText' },
+                        name: { title: '字段名称', type: 'string' },
+                        default2: { title: '默认值', type: 'string' },
+                        min: { title: '最小长度', type: 'integer', minimum: 0 },
+                        max: { title: '最大长度', type: 'integer' },
+                        enum: { title: '枚举值', type: 'array' },
+                        pattern: { title: '模式匹配', type: 'string' }
+                    }
+                },
+                {
+                    properties: {
+                        type: { const: 'text' },
+                        name: { title: '字段名称', type: 'string' },
+                        default2: { title: '默认值', type: 'string' },
+                        min: { title: '最小长度', type: 'integer', minimum: 0 },
+                        max: { title: '最大长度', type: 'integer' },
+                        enum: { title: '枚举值', type: 'array' },
+                        pattern: { title: '模式匹配', type: 'string' }
+                    }
+                },
+                {
+                    properties: {
+                        type: { const: 'bigText' },
+                        name: { title: '字段名称', type: 'string' },
+                        default2: { title: '默认值', type: 'string' },
+                        min: { title: '最小长度', type: 'integer', minimum: 0 },
+                        max: { title: '最大长度', type: 'integer' },
+                        enum: { title: '枚举值', type: 'array' },
+                        pattern: { title: '模式匹配', type: 'string' }
+                    }
+                },
+                {
+                    properties: {
+                        type: { const: 'tinyInt' },
+                        name: { title: '字段名称', type: 'string' },
+                        default: { title: '默认值', type: 'integer' },
+                        isIndex: { title: '是否索引', type: 'boolean', default: false },
+                        isUnique: { title: '是否唯一', type: 'boolean', default: false },
+                        isUnsigned: { title: '是否无符号', type: 'boolean', default: false },
+                        default2: { title: '默认值', type: 'integer' },
+                        min: { title: '最小长度', type: 'integer', minimum: 0 },
+                        max: { title: '最大长度', type: 'integer' },
+                        enum: { title: '枚举值', type: 'array' },
+                        multipleOf: { title: '倍数', type: 'integer', minimum: 1 }
+                    }
+                },
+                {
+                    properties: {
+                        type: { const: 'smallInt' },
+                        name: { title: '字段名称', type: 'string' },
+                        default: { title: '默认值', type: 'integer' },
+                        isIndex: { title: '是否索引', type: 'boolean', default: false },
+                        isUnique: { title: '是否唯一', type: 'boolean', default: false },
+                        isUnsigned: { title: '是否无符号', type: 'boolean', default: false },
+                        default2: { title: '默认值', type: 'integer' },
+                        min: { title: '最小长度', type: 'integer', minimum: 0 },
+                        max: { title: '最大长度', type: 'integer' },
+                        enum: { title: '枚举值', type: 'array' },
+                        multipleOf: { title: '倍数', type: 'integer', minimum: 1 }
+                    }
+                },
+                {
+                    properties: {
+                        type: { const: 'int' },
+                        name: { title: '字段名称', type: 'string' },
+                        default: { title: '默认值', type: 'integer' },
+                        isIndex: { title: '是否索引', type: 'boolean', default: false },
+                        isUnique: { title: '是否唯一', type: 'boolean', default: false },
+                        isUnsigned: { title: '是否无符号', type: 'boolean', default: false },
+                        default2: { title: '默认值', type: 'integer' },
+                        min: { title: '最小长度', type: 'integer', minimum: 0 },
+                        max: { title: '最大长度', type: 'integer' },
+                        enum: { title: '枚举值', type: 'array' },
+                        multipleOf: { title: '倍数', type: 'integer', minimum: 1 }
+                    }
+                },
+                {
+                    properties: {
+                        type: { const: 'mediumInt' },
+                        name: { title: '字段名称', type: 'string' },
+                        default: { title: '默认值', type: 'integer' },
+                        isIndex: { title: '是否索引', type: 'boolean', default: false },
+                        isUnique: { title: '是否唯一', type: 'boolean', default: false },
+                        isUnsigned: { title: '是否无符号', type: 'boolean', default: false },
+                        default2: { title: '默认值', type: 'integer' },
+                        min: { title: '最小长度', type: 'integer', minimum: 0 },
+                        max: { title: '最大长度', type: 'integer' },
+                        enum: { title: '枚举值', type: 'array' },
+                        multipleOf: { title: '倍数', type: 'integer', minimum: 1 }
+                    }
+                },
+                {
+                    properties: {
+                        type: { const: 'bigInt' },
+                        name: { title: '字段名称', type: 'string' },
+                        default: { title: '默认值', type: 'integer' },
+                        isIndex: { title: '是否索引', type: 'boolean', default: false },
+                        isUnique: { title: '是否唯一', type: 'boolean', default: false },
+                        isUnsigned: { title: '是否无符号', type: 'boolean', default: false },
+                        default2: { title: '默认值', type: 'integer' },
+                        min: { title: '最小长度', type: 'integer', minimum: 0 },
+                        max: { title: '最大长度', type: 'integer' },
+                        enum: { title: '枚举值', type: 'array' },
+                        multipleOf: { title: '倍数', type: 'integer', minimum: 1 }
+                    }
+                },
+                {
+                    properties: {
+                        type: { const: 'float' },
+                        name: { title: '字段名称', type: 'string' },
+                        default: { title: '默认值', type: 'number' },
                         isIndex: { title: '是否索引', type: 'boolean', default: false },
                         isUnique: { title: '是否唯一', type: 'boolean', default: false },
                         isUnsigned: { title: '是否无符号', type: 'boolean', default: false },
                         precision: { title: '整数精度', type: 'integer', minimum: 0, default: 8 },
-                        scale: { title: '小数精度', type: 'integer', minimum: 0, default: 2 }
-                    },
-                    additionalProperties: false
-                },
-                schema: {
-                    type: 'object',
-                    properties: {
-                        default: { title: '默认值', type: ['string', 'integer', 'number'] },
+                        scale: { title: '小数精度', type: 'integer', minimum: 0, default: 2 },
+                        default2: { title: '默认值', type: 'number' },
                         min: { title: '最小长度', type: 'integer', minimum: 0 },
                         max: { title: '最大长度', type: 'integer' },
                         enum: { title: '枚举值', type: 'array' },
-                        pattern: { title: '模式匹配', type: 'string' },
                         multipleOf: { title: '倍数', type: 'integer', minimum: 1 }
-                    },
-                    additionalProperties: false,
-                    required: []
+                    }
+                },
+                {
+                    properties: {
+                        type: { const: 'double' },
+                        name: { title: '字段名称', type: 'string' },
+                        default: { title: '默认值', type: 'number' },
+                        isIndex: { title: '是否索引', type: 'boolean', default: false },
+                        isUnique: { title: '是否唯一', type: 'boolean', default: false },
+                        isUnsigned: { title: '是否无符号', type: 'boolean', default: false },
+                        precision: { title: '整数精度', type: 'integer', minimum: 0, default: 8 },
+                        scale: { title: '小数精度', type: 'integer', minimum: 0, default: 2 },
+                        default2: { title: '默认值', type: 'number' },
+                        min: { title: '最小长度', type: 'integer', minimum: 0 },
+                        max: { title: '最大长度', type: 'integer' },
+                        enum: { title: '枚举值', type: 'array' },
+                        multipleOf: { title: '倍数', type: 'integer', minimum: 1 }
+                    }
                 }
-            },
-            additionalProperties: false,
-            required: ['name', 'type', 'field', 'schema']
+            ]
         }
-    },
-    additionalProperties: false
+    }
 };
--- packages/yiapi/schema/tableExt.js
+++ packages/yiapi/schema/tableExt.js
@@ -2,75 +2,13 @@ import { readdirSync } from 'node:fs';
 import { resolve } from 'node:path';
 import { system } from '../system.js';
 import { toSnakeCase } from '../utils/toSnakeCase.js';
-
-const fieldTypeEnum = [
-    //
-    'string',
-    'mediumText',
-    'text',
-    'bigText',
-    'tinyInt',
-    'smallInt',
-    'mediumInt',
-    'int',
-    'bigInt',
-    'float',
-    'double'
-];
-const schemaTypeEnum = ['string', 'integer', 'number'];
+import { tableSchema } from './table.js';
 
 const sysDbFiles = readdirSync(resolve(system.yiapiDir, 'tables'));
 const propertieFields = {};
 
 sysDbFiles.forEach((file) => {
-    propertieFields[toSnakeCase('sys_' + file.replace('.js', ''))] = {
-        title: '任意字段',
-        type: 'object',
-        patternProperties: {
-            '^[a-z][a-z0-9_]*$': {
-                title: '任意字段',
-                type: 'object',
-                properties: {
-                    name: {
-                        title: '字段名称',
-                        type: 'string'
-                    },
-                    field: {
-                        type: 'object',
-                        properties: {
-                            type: { title: '类型值', type: 'string', enum: fieldTypeEnum },
-                            default: { title: '默认值', type: ['string', 'integer', 'number'] },
-                            length: { title: '内容长度', type: 'string', maxLength: 60000 },
-                            isIndex: { title: '是否索引', type: 'boolean', default: false },
-                            isUnique: { title: '是否唯一', type: 'boolean', default: false },
-                            isUnsigned: { title: '是否无符号', type: 'boolean', default: false },
-                            precision: { title: '整数精度', type: 'integer', minimum: 0, default: 8 },
-                            scale: { title: '小数精度', type: 'integer', minimum: 0, default: 2 }
-                        },
-                        additionalProperties: false,
-                        required: ['type']
-                    },
-                    schema: {
-                        type: 'object',
-                        properties: {
-                            type: { title: '类型值', type: 'string', enum: schemaTypeEnum },
-                            default: { title: '默认值', type: ['string', 'integer', 'number', 'array'] },
-                            min: { title: '最小长度', type: 'integer', minimum: 0 },
-                            max: { title: '最大长度', type: 'integer' },
-                            enum: { title: '枚举值', type: 'array' },
-                            pattern: { title: '模式匹配', type: 'string' },
-                            multipleOf: { title: '倍数', type: 'integer', minimum: 1 }
-                        },
-                        additionalProperties: false,
-                        required: ['type']
-                    }
-                },
-                additionalProperties: false,
-                required: ['name', 'field', 'schema']
-            }
-        },
-        additionalProperties: false
-    };
+    propertieFields[toSnakeCase('sys_' + file.replace('.js', ''))] = tableSchema;
 });
 
 export const tableExtSchema = {
