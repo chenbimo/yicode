aa7db507e60b5f9cd9d78453f9814b2d5f0f2be8
chensuiyi <chenbimo@gmail.com>
2024-06-03 12:27:56 +0800

    调整配置(yiapi): 补充接口名称

--- packages/yiapi/apis/tool/_meta.js
+++ packages/yiapi/apis/tool/_meta.js
@@ -3,6 +3,7 @@ export const metaConfig = {
     apiNames: {
         sendMail: '发送邮件',
         tokenCheck: '令牌检测',
-        online: '在线统计'
+        getOnline: '获取在线统计',
+        setOnline: '设置在线统计'
     }
 };
