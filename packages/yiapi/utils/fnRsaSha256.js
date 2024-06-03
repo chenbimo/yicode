4c2761dcc978efc2be789c03a6430c522022b846
chensuiyi <chenbimo@gmail.com>
2024-06-03 12:16:51 +0800

    修复问题(yiapi): 修复fnRsa256问题

--- packages/yiapi/utils/fnRsaSha256.js
+++ packages/yiapi/utils/fnRsaSha256.js
@@ -1,6 +1,7 @@
+import { createSign } from 'node:crypto';
 // rsa-sha256 加密
 export function fnRsaSha256(data, privateKey) {
-    const sign = crypto.createSign('RSA-SHA256');
+    const sign = createSign('RSA-SHA256');
     sign.update(data);
     const signature = sign.sign(privateKey, 'base64');
     return signature;
