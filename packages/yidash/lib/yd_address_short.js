b3ef407d05728382ee29e4394d614947699175df
chensuiyi <chenbimo@gmail.com>
2024-07-13 15:56:22 +0800

    完善功能(yidash): 完善yd_address_short方法

--- packages/yidash/lib/yd_address_short.js
+++ packages/yidash/lib/yd_address_short.js
@@ -1,14 +1,16 @@
 /**
  * 地址转换为短地址形式
  */
-export function yd_address_short(address) {
+export function yd_address_short(address, startNum = 4, endNum = 4) {
     if (!address) return '';
-    const addressSplit = address.split('');
+    const addressStr = String(address);
+    if (addressStr.length < startNum + endNum) return addressStr;
+    const addressSplit = addressStr.split('');
     const result = [
         //
-        addressSplit.slice(0, 4).join(''),
-        '...',
-        addressSplit.slice(-4).join('')
+        addressSplit.slice(0, startNum).join(''),
+        '***',
+        addressSplit.slice(-endNum).join('')
     ].join('');
 
     return result;
