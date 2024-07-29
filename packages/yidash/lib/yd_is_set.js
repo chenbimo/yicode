1b2473856cf69e9556ca41b6fddea4620a4c6281
chensuiyi <chenbimo@gmail.com>
2024-07-30 00:24:47 +0800

    新增文件(yidash): 增加yd_is_set函数

--- packages/yidash/lib/yd_is_set.js
+++ packages/yidash/lib/yd_is_set.js
@@ -1,4 +1,4 @@
 import { yd_core_getTag } from './yd_core_getTag.js';
 export const yd_is_set = (value) => {
-    return yd_core_getTag(value) === ['object Set'];
+    return yd_core_getTag(value) === '[object Set]';
 };
