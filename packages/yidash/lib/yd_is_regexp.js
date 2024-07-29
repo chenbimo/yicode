9fa1918aa1e6bbbba075575f5a2341f4764e154d
chensuiyi <chenbimo@gmail.com>
2024-07-30 00:24:26 +0800

    新增文件(yidash): 增加yd_is_regexp函数

--- packages/yidash/lib/yd_is_regexp.js
+++ packages/yidash/lib/yd_is_regexp.js
@@ -1,4 +1,4 @@
 import { yd_core_getTag } from './yd_core_getTag.js';
 export const yd_is_regexp = (value) => {
-    return yd_core_getTag(value) === ['object RegExp'];
+    return yd_core_getTag(value) === '[object RegExp]';
 };
