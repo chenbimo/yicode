176eb2bc38a978e6bc20dadeb1837242bbba5868
chensuiyi <chenbimo@gmail.com>
2024-07-30 00:25:16 +0800

    新增文件(yidash): 增加yd_is_weekMap函数

--- packages/yidash/lib/yd_is_weakMap.js
+++ packages/yidash/lib/yd_is_weakMap.js
@@ -1,4 +1,4 @@
 import { yd_core_getTag } from './yd_core_getTag.js';
 export const yd_is_weakMap = (value) => {
-    return yd_core_getTag(value) === ['object WeakMap'];
+    return yd_core_getTag(value) === '[object WeakMap]';
 };
