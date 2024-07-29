e7d7d2cfdff052197d4837e61a80b09e7ee89512
chensuiyi <chenbimo@gmail.com>
2024-07-29 17:11:50 +0800

    完善功能(tnbc): 更新依赖，重构代码

---
+++ packages/tnbc/README.md
@@ -0,0 +1,7 @@
+# tnb（taobao npmrc binary）淘宝NPM配置二进制下载地址
+
+全局安装后，执行 `tnb` 回车即可。
+
+## 参考
+
+-   https://github.com/cnpm/binary-mirror-config
---
+++ packages/tnbc/index.js
@@ -0,0 +1,48 @@
+#!/usr/bin/env node
+import url from 'node:url';
+import { join } from 'node:path';
+import { env, platform } from 'node:process';
+import { readFileSync, writeFileSync } from 'node:fs';
+
+// 配置处理包
+import { parse as ini_parse, stringify as ini_stringify } from 'ini';
+import { ensureFileSync } from 'fs-extra/esm';
+import logSymbols from 'log-symbols';
+
+// NPM 配置地址
+const npmrc = join(env[platform === 'win32' ? 'USERPROFILE' : 'HOME'], '.npmrc');
+
+// 镜像地址
+const taobaoMirror = 'https://npmmirror.com/mirrors/';
+
+// 包映射
+const packageMap = {
+    SHARP_BINARY_HOST: `${taobaoMirror}sharp/`,
+    SHARP_LIBVIPS_BINARY_HOST: `${taobaoMirror}sharp-libvips/`,
+    CHROMEDRIVER_CDNURL: `${taobaoMirror}chromedriver/`,
+    ELECTRON_MIRROR: `${taobaoMirror}electron/`,
+    ELECTRON_CUSTOM_DIR: `{{ version }}`,
+    ELECTRON_SKIP_BINARY_DOWNLOAD: 1,
+    SASS_BINARY_SITE: `${taobaoMirror}node-sass`
+};
+
+// 获取当前npmrc配置
+const mainFun = () => {
+    ensureFileSync(npmrc);
+    const fileData = readFileSync(npmrc, 'utf-8');
+    const fileConfig = ini_parse(fileData);
+    const fileKeys = Object.keys(fileConfig).map((name) => name.toUpperCase());
+    let newFileConfig = {};
+    for (let key in fileConfig) {
+        let keyUpper = key.toUpperCase();
+        if (packageMap[keyUpper]) {
+            newFileConfig[keyUpper] = packageMap[keyUpper];
+        } else {
+            newFileConfig[key] = fileConfig[key];
+        }
+    }
+    writeFileSync(npmrc, ini_stringify(newFileConfig));
+    console.log(`${logSymbols.success} NPM二进制下载链接设置成功`);
+};
+
+mainFun();
