2fcfd2f5fc45852a2ac84e0e6a66c6488e6db6ce
chensuiyi <chenbimo@gmail.com>
2024-07-30 00:25:23 +0800

    新增文件(yidash): 增加yd_is_weekSet函数

--- packages/yidash/lib/yd_is_weakSet.js
+++ packages/yidash/lib/yd_is_weakSet.js
@@ -1,4 +1,4 @@
 import { yd_core_getTag } from './yd_core_getTag.js';
 export const yd_is_weakSet = (value) => {
-    return yd_core_getTag(value) === ['object WeakSet'];
+    return yd_core_getTag(value) === '[object WeakSet]';
 };
