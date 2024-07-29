a7c280a97530830bb2f094a1f72e3ca6eea0f700
chensuiyi <chenbimo@gmail.com>
2024-07-30 00:22:23 +0800

    新增文件(yidash): 增加yd_is_boolean函数

--- packages/yidash/lib/yd_is_boolean.js
+++ packages/yidash/lib/yd_is_boolean.js
@@ -1,4 +1,4 @@
 import { yd_core_getTag } from './yd_core_getTag.js';
 export const yd_is_boolean = (value) => {
-    return value === true || value === false || yd_core_getTag(value) === ['object Boolean'];
+    return value === true || value === false || yd_core_getTag(value) === '[object Boolean]';
 };
